60ff0b04d3ada8343a790e3b2fcf4ae5b2e8da13
VAL\NallanA <nallana@valassis.com>
2017-11-27 13:19:25 -0500

    US-6087 Search data and service request

--- src/app/components/business-search/business-search.component.ts
+++ src/app/components/business-search/business-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../services/app.service';
+import {DropdownModule} from 'primeng/primeng';
 
 
 @Component({
@@ -11,20 +12,102 @@ import { AppService } from '../../services/app.service';
 export class BusinessSearchComponent implements OnInit {
   public name: string;  // Used by parent as a header
   public numFound: number = 2;
-
+  dropdownList: any[];
+  
+  selectedCategory: string;
+  
   // As we wire the component up to real sources, we can remove the below
   selectedCity: string;
+  model: any = {};
   sourceCategories: any = [];
   targetCategories: any = [];
+  filteredCategories: any = [];
   geofootprintGeos: any;
   competitors: any;
   sites: any;
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService) { 
+    this.dropdownList = [  {name:'Apparel & Accessory Stores'},
+    {name: 'Building Materials & Hardware'}, 
+    {name: 'General Merchandise Stores'}, 
+    {name:'Food Stores'},
+    {name:'Automotive Dealers & Service Stations'},
+    {name:'Home Furniture & Furnishings Stores'},
+    {name:'Eating & Drinking Places'},
+    {name:'Miscellaneous Retail'},
+    {name:'Depository Institutions'},
+    {name:'Personal Services'},
+    {name:'Auto Services'},
+    {name:'Leisure Services'},
+    {name:'Dentists & Doctors'},
+    {name:'Schools & Universities'}
+];
+
+  }
 
   ngOnInit() {
     this.name = 'Business Search';
     this.sourceCategories = this.appService.categoriesList;
+    this.filteredCategories = this.appService.categoriesList;
+  }
+  assignCopy(){
+    this.sourceCategories = Object.assign([], this.filteredCategories);
+  }
+  filterCategory(value) {
+    if (!value){
+      this.assignCopy();
+    }
+    this.sourceCategories = Object.assign([], this.filteredCategories).filter((item) => {
+      return item.name ? (item.name.toLowerCase().indexOf(value.toLowerCase()) > -1) : false;
+    })
+  }
+
+  onSearchBusiness(){
+    let paramObj = {
+      "sites": [
+          {
+              "x": "-117.351856",
+              "y": "34.470195"
+          },
+          {
+              "x": "-117.156703",
+              "y": "33.684842"
+          },
+          {
+              "x": "-84.30181",
+              "y": "33.898628"
+          }
+          
+      ],
+      
+    // "radius" : "3",
+    // "name" : "INSTITUTE",
+    // "city" : "ST LOUIS",
+    // "state" : "MO",
+    // "zip" : "63127",
+    // "countyName" : "SAINT LOUIS",
+    // "eliminateBlankFirmNames" : "True",
+    // "siteLimit" : "200"
+    // };
+
+      "radius": this.model.radius,
+      "name": this.model.name,
+      "city": this.model.city,
+      "state": this.model.state,
+      "zip": this.model.zip,
+      "countyName": this.model.countyName,
+      "eliminateBlankFirmNames": "True",
+      "siteLimit": "200"
+  };
+  paramObj['sics'] = this.targetCategories.map((obj) => {
+    return {
+      'sic': obj.sic
+    }
+  });
+  console.log(paramObj);
+    this.appService.getbusinesses(paramObj).subscribe((data) => {
+      console.log('returnData'+ data.payload);
+    });
   }
 
 }
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -8,17 +8,18 @@ import 'rxjs/add/operator/map';
 export class AppService {
 
     public categoriesList = [
-        {name: 'CLINICS', primarysic: '8011-04'},
-        {name: 'PHYSICIANS & SURGEONS-EMERGENCY SERVICE', primarysic: '8011-05'},
-        {name:'PSYCHOANALYSTS', primarysic: '8011-06'},
-        {name:'TATTOOS-REMOVED', primarysic: '8011-07'},
-        {name:'ANESTHETISTS', primarysic: '8011-08'},
-        {name:'OPHTHALMOLOGISTS', primarysic: '8011-11'},
-        {name:'PSYCHIATRY-ADULT CHILD & ADOLESCENT', primarysic: '8011-13'},
-        {name:'PHYSICIANS & SURGEONS', primarysic: '8011-01'},
-        {name:'SPORTS MEDICINE & INJURIES', primarysic: '8011-02'},
-        {name:'ALLERGY PHYSICIANS', primarysic: '8011-20'},
-        {name:'INDIAN GOODS', primarysic: '5947-09'},
+        {name: 'CLINICS', sic: '8011-04'},
+        {name: 'PHYSICIANS & SURGEONS-EMERGENCY SERVICE', sic: '8011-05'},
+        {name:'PSYCHOANALYSTS', sic: '8011-06'},
+        {name:'TATTOOS-REMOVED', sic: '8011-07'},
+        {name:'ANESTHETISTS', sic: '8011-08'},
+        {name:'OPHTHALMOLOGISTS', sic: '8011-11'},
+        {name:'PSYCHIATRY-ADULT CHILD & ADOLESCENT', sic: '8011-13'},
+        {name:'PHYSICIANS & SURGEONS', sic: '8011-01'},
+        {name:'SPORTS MEDICINE & INJURIES', sic: '8011-02'},
+        {name:'ALLERGY PHYSICIANS', sic: '8011-20'},
+        {name:'INDIAN GOODS', sic: '5947-09'},
+        {name:'PIZZA', sic:'5812-22'}
     ]
 
     // public readonly categoryList = 'assets/categories.json'; 
@@ -29,48 +30,15 @@ export class AppService {
     //         .map((resp: Response) => resp.json());
     // }
 
-    private readonly searchbusiness = 'https://services.valassislab.com/services/v1/mediaexpress/base/targetingsearch/search';
+    private readonly searchbusiness = 'https://servicesdev.valassislab.com/services/v1/targeting/base/targetingsearch/search';
     
         constructor(private http: Http) { }
-        public getbusinesses(): Observable<any> {
-
-            let paramObj = {
-                "sites": [
-                    {
-                        "x": "-117.351856",
-                        "y": "34.470195"
-                    },
-                    {
-                        "x": "-117.156703",
-                        "y": "33.684842"
-                    },
-                    {
-                        "x": "-84.30181",
-                        "y": "33.898628"
-                    }
-                    
-                ],
-                "sics": [
-                    {
-                        "sic": "8011-01"
-                    },
-                    {
-                        "sic": "8011-05"
-                    }
-                ],
-                "radius": "3",
-                "name": "INSTITUTE",
-                "city": "ST LOUIS",
-                "state": "MO",
-                "zip": "63127",
-                "countyName": "SAINT LOUIS",
-                "eliminateBlankFirmNames": "True",
-                "siteLimit": "200"
-            };
-
+        public getbusinesses(paramObj): Observable<any> {
+        
             return this.http.post(`${this.searchbusiness}`, paramObj)
                 .map((resp: Response) => resp.json());
         }
+        
 
 }
 
