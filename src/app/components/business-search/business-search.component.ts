59eaa2d91bb9209fa27d8efe7f8a49b54fe58844
VAL\NallanA <nallana@valassis.com>
2017-11-28 22:13:29 -0500

    US6087: Business Search Component Ui Fix - with data from targeting/base service

--- src/app/components/business-search/business-search.component.ts
+++ src/app/components/business-search/business-search.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AppService } from '../../services/app.service';
 import { DropdownModule } from 'primeng/primeng';
 import { MapService } from '../../services/map.service';
 
 @Component({
-  providers: [AppService,MapService],
+  providers: [AppService, MapService],
   selector: 'val-business-search',
   templateUrl: './business-search.component.html',
   styleUrls: ['./business-search.component.css']
 })
 export class BusinessSearchComponent implements OnInit {
+  @Output()
+  showSideBar: EventEmitter<any> = new EventEmitter<any>();
   public name: string;  // Used by parent as a header
-  public numFound: number = 2;
+  public numFound: number;
   dropdownList: any[];
 
   selectedCategory: string;
@@ -25,6 +27,7 @@ export class BusinessSearchComponent implements OnInit {
   geofootprintGeos: any;
   competitors: any;
   sites: any;
+
 
   constructor(private appService: AppService, private mapService: MapService) {
     this.dropdownList = [{ label: 'Apparel & Accessory Stores' },
@@ -78,52 +81,54 @@ export class BusinessSearchComponent implements OnInit {
 
       ],
 
-      "radius": "3",
-      "name": "INSTITUTE",
-      "city": "ST LOUIS",
-      "state": "MO",
-      "zip": "63127",
-      "countyName": "SAINT LOUIS",
-      "eliminateBlankFirmNames": "True",
-      "siteLimit": "200"
-    };
-
-    //     "radius": this.model.radius,
-    //     "name": this.model.name,
-    //     "city": this.model.city,
-    //     "state": this.model.state,
-    //     "zip": this.model.zip,
-    //     "countyName": this.model.countyName,
-    //     "eliminateBlankFirmNames": "True",
-    //     "siteLimit": "200"
+    //   "radius": "3",
+    //   "name": "INSTITUTE",
+    //   "city": "ST LOUIS",
+    //   "state": "MO",
+    //   "zip": "63127",
+    //   "countyName": "SAINT LOUIS",
+    //   "eliminateBlankFirmNames": "True",
+    //   "siteLimit": "200"
     // };
+
+        "radius": this.model.radius,
+        "name": this.model.name,
+        "city": this.model.city,
+        "state": this.model.state,
+        "zip": this.model.zip,
+        "countyName": this.model.countyName,
+        "eliminateBlankFirmNames": "True",
+        "siteLimit": "200"
+    };
 
     paramObj['sics'] = this.targetCategories.map((obj) => {
       return {
         'sic': obj.sic
       }
-    });
-    
-      console.log("request to business search"+paramObj);
-  
+    }); 
+
+    console.log("request to business search" + paramObj);
 
       var observable = this.appService.businessSearch(paramObj);
-     
+
       observable.subscribe((res) => {
       var any = res.payload;
       console.log("In Business Search  componenet GOT ROWS : " + JSON.stringify(any.rows, null, 4));
       this.searchDatageos = any.rows;
     });
+    
   }
 
   onAddToProject() {
-    let finalData = this.searchDatageos.map((obj) => {
-      if (obj.checked) {
-        return { x: obj.x, y: obj.y }
+    //this.showSideBar.emit({show: false, x: -90.381958, y:38.530309});
+    // setTimeout(() => {
+    //   this.mapService.plotMarker(-90.381958, 38.530309);
+    // });
+    this.showSideBar.emit(false);
+    this.searchDatageos.forEach(business => {
+      if (business.checked && business.checked.length > 0) {
+        this.mapService.plotMarker(business.x, business.y);
       }
     });
-    //this.mapService.plotMarker(finalData.x, finalData.y);
-    console.log(finalData);
   }
-
 }
