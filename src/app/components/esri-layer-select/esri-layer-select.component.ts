6a66f78d4affeeca7e4971e48f6b23aff81c901c
Todd Faucheux <tjfauche@valassis.com>
2017-11-30 17:47:29 -0500

    updated layer selection to multi-select, and updated map service

--- src/app/components/esri-layer-select/esri-layer-select.component.ts
+++ src/app/components/esri-layer-select/esri-layer-select.component.ts
@@ -10,6 +10,10 @@ import { EsriLoaderService } from 'angular-esri-loader';
 // Import Core Modules
 import { CONFIG, MessageService } from '../../core';
 
+// import primeng 
+import {SelectItem} from 'primeng/primeng';
+// import {ButtonModule} from 'primeng/primeng';
+
 @Component({
   providers: [MapService],
   selector: 'val-esri-layer-select',
@@ -19,51 +23,50 @@ import { CONFIG, MessageService } from '../../core';
 export class EsriLayerSelectComponent implements OnInit {
 
   // MapService Items
-  private selectedService: string;
-  private s_name: string;
-  private s_url: string;
-
-  // public msgs: Message[] = [];
-  public esriDemographicItems: any[];
   public mapView: __esri.MapView;
   private esriMap: __esri.Map;
 
   // this is needed to be able to create the MapView at the DOM element in this component
   @ViewChild('mapViewNode') private mapViewEl: ElementRef;
 
-  constructor(private mapService: MapService
-              // private messageService: MessageService
-             ) {
+  public esriDemographicItems: SelectItem[];
+  selectedLayers: string[] = [];
+
+  constructor(private mapService: MapService) {
      this.mapView = this.mapService.getMapView();
-  }
+    }
 
   public ngOnInit() {
     try {
       this.esriDemographicItems = [
-            {name: '<Blank>'                            , code: ''},
-            {name: 'ATZ_Top_Vars_Portal'                , code: 'https://valvcshad001vm.val.vlss.local/server/rest/services/ATZ_Top_Vars_Portal_ReferenceRegisteredData/MapServer'},
-            {name: 'ATZ_Centroids_Portal'               , code: 'https://valvcshad001vm.val.vlss.local/server/rest/services/ATZ_Centroids_Portal_ReferenceRegisteredData/MapServer'},
-            {name: 'USA_1990-2000_Population_Change'    , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_1990-2000_Population_Change/MapServer'},
-            {name: 'USA_2000-2010_Population_Change'    , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_2000-2010_Population_Change/MapServer'},
-            {name: 'USA_Average_Household_Size'         , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Average_Household_Size/MapServer'},
-            {name: 'USA_Diversity_Index'                , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Diversity_Index/MapServer'},
-            {name: 'USA_Labor_Force_Participation_Rate' , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Labor_Force_Participation_Rate/MapServer'},
-            {name: 'USA_Median_Age'                     , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Age/MapServer'},
-            {name: 'USA_Median_Home_Value'              , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Home_Value/MapServer'},
-            {name: 'USA_Median_Household_Income'        , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Household_Income/MapServer'},
-            {name: 'USA_Median_Net_Worth'               , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Net_Worth/MapServer'},
-            {name: 'USA_Owner_Occupied_Housing'         , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Owner_Occupied_Housing/MapServer'},
-            {name: 'USA_Percent_Over_64'                , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Percent_Over_64/MapServer'},
-            {name: 'USA_Percent_Under_18'               , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Percent_Under_18/MapServer'},
-            {name: 'USA_Population_by_Sex'              , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_by_Sex/MapServer'},
-            {name: 'USA_Population_Density'             , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_Density/MapServer'},
-            {name: 'USA_Projected_Population_Change'    , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Projected_Population_Change/MapServer'},
-            {name: 'USA_Recent_Population_Change'       , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Recent_Population_Change/MapServer'},
-            {name: 'USA_Retail_Spending_Potential'      , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Retail_Spending_Potential/MapServer'},
-            {name: 'USA_Social_Vulnerability_Index'     , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Social_Vulnerability_Index/MapServer'},
-            {name: 'USA_Tapestry'                       , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Tapestry/MapServer'},
-            {name: 'USA_Unemployment_Rate'              , code: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Unemployment_Rate/MapServer'},
-            {name: 'Census_2000'                        , code: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer'}
+            {label: 'Digital_ATZ'                        , value: 'https://services7.arcgis.com/U1jwgAVNb50RuY1A/ArcGIS/rest/services/digitalATZ/FeatureServer'},
+            {label: 'ZIP_Top_Vars'                       , value: 'https://services7.arcgis.com/U1jwgAVNb50RuY1A/ArcGIS/rest/services/ZIP_Top_Vars/FeatureServer'},
+            {label: 'ATZ_Top_Vars'                       , value: 'https://services7.arcgis.com/U1jwgAVNb50RuY1A/arcgis/rest/services/ATZ_Top_Vars/FeatureServer'},
+            {label: 'ZIP_Centroids'                      , value: 'https://services7.arcgis.com/U1jwgAVNb50RuY1A/ArcGIS/rest/services/ZIP_Centroids/FeatureServer'},
+            {label: 'ATZ_Centroids'                      , value: 'https://services7.arcgis.com/U1jwgAVNb50RuY1A/ArcGIS/rest/services/ATZ_Centroids/FeatureServer'},
+        // -----------------
+            {label: 'Census_2000'                        , value: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer'},
+        // -----------------
+            {label: 'USA_1990-2000_Population_Change'    , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_1990-2000_Population_Change/MapServer'},
+            {label: 'USA_2000-2010_Population_Change'    , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_2000-2010_Population_Change/MapServer'},
+            {label: 'USA_Average_Household_Size'         , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Average_Household_Size/MapServer'},
+            {label: 'USA_Diversity_Index'                , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Diversity_Index/MapServer'},
+            {label: 'USA_Labor_Force_Participation_Rate' , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Labor_Force_Participation_Rate/MapServer'},
+            {label: 'USA_Median_Age'                     , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Age/MapServer'},
+            {label: 'USA_Median_Home_Value'              , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Home_Value/MapServer'},
+            {label: 'USA_Median_Household_Income'        , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Household_Income/MapServer'},
+            {label: 'USA_Median_Net_Worth'               , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Net_Worth/MapServer'},
+            {label: 'USA_Owner_Occupied_Housing'         , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Owner_Occupied_Housing/MapServer'},
+            {label: 'USA_Percent_Over_64'                , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Percent_Over_64/MapServer'},
+            {label: 'USA_Percent_Under_18'               , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Percent_Under_18/MapServer'},
+            {label: 'USA_Population_by_Sex'              , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_by_Sex/MapServer'},
+            {label: 'USA_Population_Density'             , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Population_Density/MapServer'},
+            {label: 'USA_Projected_Population_Change'    , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Projected_Population_Change/MapServer'},
+            {label: 'USA_Recent_Population_Change'       , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Recent_Population_Change/MapServer'},
+            {label: 'USA_Retail_Spending_Potential'      , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Retail_Spending_Potential/MapServer'},
+            {label: 'USA_Social_Vulnerability_Index'     , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Social_Vulnerability_Index/MapServer'},
+            {label: 'USA_Tapestry'                       , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Tapestry/MapServer'},
+            {label: 'USA_Unemployment_Rate'              , value: 'https://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Unemployment_Rate/MapServer'}
         ];
     }
     // tslint:disable-next-line:one-line
@@ -72,37 +75,12 @@ export class EsriLayerSelectComponent implements OnInit {
     }
   }
 
-   onChangeMapService(dd: any) {
-            
-            this.s_name = dd.selectedOption.value.name; this.s_url = dd.selectedOption.value.code;
-            console.log(this.s_name, this.s_url);
+   onRemoveMapServices() {
+     this.mapService.removeMapLayers();
+    }
 
-            //add feature layer
-            // this.msgs.addSingle('info','Loading Layer', this.s_name);
-            // this.msgs.push({severity:'info', summary:'Info Message', detail:'PrimeNG rocks'});
-            this.mapService.setMapLayer(this.s_name, this.s_url, 'FeatureLayer');
-/*
-
-                //wait until the layer is loaded.
-                layer.then
-                    (() =>
-                    {
-                      let toc = document.getElementById("toc");
-                      toc.innerHTML = "";
-                      let layerlist = document.createElement("ul");
-                      toc.appendChild(layerlist);
-                      //populate layers in list
-                      populateLayerRecursive(layer, layerlist);
-                      //mapview.goTo(layer.fullExtent)
-
-                      mapview.goTo(results.features, {animate: false}).then(function(response) {
-                      let zoomView = {};
-                      zoomView = mapview.extent.expand(2.0);
-                      mapview.goTo(zoomView);
-                      });
-                    }
-                    )
-      */
+   onSetMapServices() {
+     this.mapService.setMapLayers(this.selectedLayers);
     }
 
 }
