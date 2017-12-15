b628c0e83cc61ffe58212a46bb3f09dfc577cfc7
David Gegenheimer <davidgeg@gmail.com>
2017-12-15 18:36:58 +0000

    US6419 - Responsive fonts and icons for metrics boxes.

---
+++ src/app/val-modules/common/components/message.component.ts
@@ -0,0 +1,57 @@
+import {Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {map } from 'rxjs/operators';
+import {Observable } from 'rxjs/Observable';
+import {Message } from '../models/Message';
+import {MessageService } from '../services/message.service';
+
+@Component({
+  selector: 'val-message-component',
+  template: `
+    <div>
+      <form [formGroup]="messageForm" (submit)="onSubmit()">
+         <button>Add Message</button><br />
+      </form>
+      
+      <p>
+        {{singleMessage$ | async | json}}
+      </p>
+      
+      <div *ngFor="let message of messages | async">
+        {{ message.value }} <button (click)="deleteMessage(message.id)">x</button>
+      </div>
+    </div>
+  `
+})
+export class MessageComponent implements OnInit
+{
+  messages: Observable<Message[]>;
+  singleMessage$: Observable<Message>;
+  
+  constructor(
+    private messageService: MessageService, 
+    private formBuilder: FormBuilder) {
+      
+    this.messageForm = this.formBuilder.group({
+      'todo': ['', Validators.required]
+    });
+  }
+  
+  ngOnInit() {
+    this.messages = this.messageService.messages;
+    this.singleMessage$ = this.messageService.messages.pipe(
+      map(todos => messages.find(item => item.id === '1'))
+    );
+    
+    this.messageService.loadAll();
+    this.messageService.load('1');
+  }
+  
+  onSubmit() {
+    this.messageService.create({ value: this.messageForm.controls.message.value });
+  }
+  
+  deleteTodo(todoId: number) {
+    this.todoService.remove(todoId);
+  }
+}
