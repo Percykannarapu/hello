50d0992c947395187928de4c125985b063b16fe4
Aaron Cirillo <amcirillo@valassis.com>
2018-01-03 17:13:51 -0500

    US6451: modified geocoder service to use HTTPS

--- src/app/services/geocoder.service.ts
+++ src/app/services/geocoder.service.ts
@@ -28,14 +28,14 @@ export class GeocoderService {
       state: amSite.state,
       postalCode: Number(amSite.zip)
     };
-    return this.http.post('http://vallomjbs002vm/services/v1/geocoder/singlesite', accountLocation).map(res => res.json() as RestResponse);
+    return this.http.post('https://servicesdev.valassislab.com/services/v1/geocoder/singlesite', accountLocation).map(res => res.json() as RestResponse);
   }
   
   saveGeofootprintMaster(geofootprintMaster: GeofootprintMaster){
     //JSON mapper = new JSON();
     console.log('fired saveGeofootprintMaster in GeocoderService ' + JSON.stringify(geofootprintMaster, null, 4));
      
-    return this.http.post('http://servicesdev.valassislab.com/services/v1/mediaexpress/base/geofootprintmaster/save', geofootprintMaster).map(res => res.json() as RestResponse);
+    return this.http.post('https://servicesdev.valassislab.com/services/v1/mediaexpress/base/geofootprintmaster/save', geofootprintMaster).map(res => res.json() as RestResponse);
   }
 
 }
