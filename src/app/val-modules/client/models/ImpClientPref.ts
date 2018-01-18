7ea83c129a7aa483dcf5a30396cc47dd46733a0d
David Gegenheimer <davidgeg@gmail.com>
2018-01-18 18:56:51 +0000

    US6444 - fixed client domain folder structure

---
+++ src/app/val-modules/client/models/ImpClientPref.ts
@@ -0,0 +1,75 @@
+/** A CLIENT domain class representing the table: IMPOWER.IMP_CLIENT_PREFS */
+import { ClientIdentifierType } from '../../mediaexpress/models/ClientIdentifierType';
+import { ImpProjectPref } from './ImpProjectPref';
+
+export class ImpClientPref
+{
+   public clientPrefId:                number;                    /// Primary Key
+   public createUser:                  number;                   
+   public createDate:                  Date;                     
+   public modifyUser:                  number;                   
+   public modifyDate:                  Date;                     
+   public clientIdentifierId:          number;                   
+   public attributeCode:               string;                   
+   public attributeType:               string;                   
+   public attributeValue:              string;                   
+   public isActive:                    number;                   
+
+   // IMPOWER.IMP_CLIENT_PREFS - MANY TO ONE RELATIONSHIP MEMBERS
+   // -----------------------------------------------------------
+   public clientIdentifierTypeCode:    ClientIdentifierType;      /// Cbx Client Identifier Types
+
+   // IMPOWER.IMP_CLIENT_PREFS - ONE TO MANY RELATIONSHIP MEMBERS
+   // -----------------------------------------------------------
+   public impProjectPrefs:             Set<ImpProjectPref>;       /// Set of impProjectPrefs related to this ImpClientPref
+
+   // Can construct without params or as ({fieldA: 'xyz', fieldB: 123});
+   constructor(data: ImpClientPref | {} = {}) {
+      Object.assign(this, data);
+   }
+
+   /**
+    * Produces a map of this classes fields and data types.
+    * Used instead of reflection, which has limitations.
+    *
+    * @returns Map<field, type>
+    */
+   public static getFields () : Map<string, string>
+   {
+      return new Map([
+         ['clientPrefId',                 'number'],
+         ['createUser',                   'number'],
+         ['createDate',                   'Date'],
+         ['modifyUser',                   'number'],
+         ['modifyDate',                   'Date'],
+         ['clientIdentifierId',           'number'],
+         ['attributeCode',                'string'],
+         ['attributeType',                'string'],
+         ['attributeValue',               'string'],
+         ['isActive',                     'number']
+         ]);
+   }
+
+   /**
+    * Produces a map of this classes relationships and data types.
+    * Used instead of reflection, which has limitations.
+    *
+    * @returns Map<field, type>
+    */
+   public static getRelationships () : Map<string, string>
+   {
+      return new Map([
+         // MANY TO ONE RELATIONSHIP MEMBERS
+         ['clientIdentifierTypeCode',     'ClientIdentifierType'],
+         // ONE TO MANY RELATIONSHIP MEMBERS
+         ['impProjectPrefs',              'Set<ImpProjectPref>']
+         ]);
+   }
+
+   /**
+    * Returns the class as a string.
+    *
+    * @returns A string containing the class data.
+    */
+   public toString = () => JSON.stringify(this, null, '   ');
+}
