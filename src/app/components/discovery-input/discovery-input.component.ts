925d69bf99d4a33a814662a0e999bcd8606b6dca
David Gegenheimer <davidgeg@gmail.com>
2018-01-19 22:24:14 +0000

    US6497 - Began adding httpclient for rad lookup

--- src/app/components/discovery-input/discovery-input.component.ts
+++ src/app/components/discovery-input/discovery-input.component.ts
@@ -1,12 +1,16 @@
+import { Observable } from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
 import {ImpProduct} from './../../val-modules/mediaplanning/models/ImpProduct';
 import {Component, OnInit} from '@angular/core';
 import {SelectItem} from 'primeng/primeng';
+import {ImpRadLookup} from './../../val-modules/targeting/models/ImpRadLookup';
 
 interface Product {
    productName: string;
    productCode: string;
 }
+
+const radDataUrl = 'https://servicesdev.valassislab.com/services/v1/targeting/base/impradlookup/search?q=impRadLookup';
 
  @Component({
   selector: 'val-discovery-input',
@@ -19,6 +23,8 @@ export class DiscoveryInputComponent implements OnInit
    productItems: SelectItem[];
 
    selectedProduct: Product;
+
+   radData: ImpRadLookup[];
 
    constructor(public http: HttpClient)
    {
@@ -40,6 +46,35 @@ export class DiscoveryInputComponent implements OnInit
    }
 
    ngOnInit() {
+      console.log('Firing off a call to impRadLookup');
+      this.http.get(radDataUrl).subscribe(data => {
+         console.log(data);
+       });
    }
 
+
+   getRadLookupData() : Observable<ImpRadLookup[]> {
+      return this.http.get(radDataUrl)
+        .do(data => console.log(data)) // view results in the console
+  //      .map(res => res.json())
+        .catch(this.handleError);
+    }
+  
+  /*
+    getGeos(): Observable <any> {
+      console.log('GfGeoService.getGeos fired');
+      return this.http.get(geofootprintGeosUrl)
+                      .subscribe*/
+  /*                    .map(response => response.json().contents)
+                      .catch((err: Response|any) => {
+                                                      return Observable.throw(err.statusText);
+                                                    });*/
+  // }
+  
+    private handleError (error: any) {
+      // In a real world app, we might send the error to remote logging infrastructure
+      // and reformat for user consumption
+      console.error(error); // log to console instead
+      return Observable.throw(error);
+    }   
 }
