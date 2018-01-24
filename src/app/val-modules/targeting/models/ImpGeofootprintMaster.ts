f44adb0887581435288f8a8eea1baa632d04f5c3
David Gegenheimer <davidgeg@gmail.com>
2018-01-24 23:52:49 +0000

    US6319 - Removed renamed classes, regenerated targeting domain

--- src/app/val-modules/targeting/models/ImpGeofootprintMaster.ts
+++ src/app/val-modules/targeting/models/ImpGeofootprintMaster.ts
@@ -7,20 +7,20 @@ import { ImpProject } from './ImpProject';
 
 export class ImpGeofootprintMaster
 {
-   public cgmId:                 number;                     /// Primary key identifying the current run for the profile.
-   public summaryInd:            number;                     /// 1 = Summary, 0 = Not summary
-   public allowDuplicate:        number;                     /// Indicator for allowing duplicate geos
-   public createdDate:           Date;                       /// Date/Time row was created
-   public status:                string;                     /// Indicates success or failure of geofootprint creation
-   public methAnalysi:           string;                     /// Method analysis level. ZIP or ATZ
-   public methSeason:            string;                     /// Season
-   public activeSiteCount:       number;                     /// Total number of active sites
-   public totalSiteCount:        number;                     /// Total number of sites
-   public isMarketBased:         number;                     /// 1 = Market based, 2 = Store based
+   public cgmId:                     number;                        /// Primary key identifying the current run for the profile.
+   public summaryInd:                number;                        /// 1 = Summary, 0 = Not summary
+   public allowDuplicate:            number;                        /// Indicator for allowing duplicate geos
+   public createdDate:               Date;                          /// Date/Time row was created
+   public status:                    string;                        /// Indicates success or failure of geofootprint creation
+   public methAnalysi:               string;                        /// Method analysis level. ZIP or ATZ
+   public methSeason:                string;                        /// Season
+   public activeLocationCount:       number;                        /// Total number of active location
+   public totalLocationCount:        number;                        /// Total number of location
+   public isMarketBased:             number;                        /// 1 = Market based, 2 = Store based
 
    // IMPOWER.IMP_GEOFOOTPRINT_MASTER - MANY TO ONE RELATIONSHIP MEMBERS
    // ------------------------------------------------------------------
-   public projectId:             ImpProject;                 /// Captures Project information from the UI
+   public impProject:                ImpProject;                    /// Captures Project information from the UI
 
    // Can construct without params or as ({fieldA: 'xyz', fieldB: 123});
    constructor(data: ImpGeofootprintMaster | {} = {}) {
@@ -36,16 +36,16 @@ export class ImpGeofootprintMaster
    public static getFields () : Map<string, string>
    {
       return new Map([
-         ['cgmId',                  'number'],
-         ['summaryInd',             'number'],
-         ['allowDuplicate',         'number'],
-         ['createdDate',            'Date'],
-         ['status',                 'string'],
-         ['methAnalysi',            'string'],
-         ['methSeason',             'string'],
-         ['activeSiteCount',        'number'],
-         ['totalSiteCount',         'number'],
-         ['isMarketBased',          'number']
+         ['cgmId',                      'number'],
+         ['summaryInd',                 'number'],
+         ['allowDuplicate',             'number'],
+         ['createdDate',                'Date'],
+         ['status',                     'string'],
+         ['methAnalysi',                'string'],
+         ['methSeason',                 'string'],
+         ['activeLocationCount',        'number'],
+         ['totalLocationCount',         'number'],
+         ['isMarketBased',              'number']
          ]);
    }
 
@@ -59,7 +59,7 @@ export class ImpGeofootprintMaster
    {
       return new Map([
          // MANY TO ONE RELATIONSHIP MEMBERS
-         ['projectId',              'ImpProject']
+         ['impProject',                 'ImpProject']
          ]);
    }
 
