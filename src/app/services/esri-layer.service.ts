152d76596a4167fb241ee42429730390fda0f83a
kirk-clawson <kirk.clawson@outlook.com>
2018-01-29 12:23:03 -0500

    US 6352 - Added shading on hard-coded variable

--- src/app/services/esri-layer.service.ts
+++ src/app/services/esri-layer.service.ts
@@ -1,16 +1,15 @@
 import {Injectable} from '@angular/core';
 import {EsriModules} from './esri-modules.service';
 
-type esriActionSections = __esri.Collection<__esri.Collection<__esri.Action>>;
-type esriActions = __esri.Collection<__esri.Action>;
-
 @Injectable()
 export class EsriLayerService {
   private mapView: __esri.MapView;
   private colorSlider: __esri.ColorSlider;
+  private showingDemoVars: boolean;
+  private currentRenderer: __esri.Renderer;
+  private currentOpacity: number;
 
-  constructor(private modules: EsriModules){
-  }
+  constructor(private modules: EsriModules){}
 
   public initLayerList(mapView: __esri.MapView) : void {
     this.mapView = mapView;
@@ -27,9 +26,9 @@ export class EsriLayerService {
     const layerList: __esri.LayerList = new EsriModules.LayerList({
       view: this.mapView,
       container: document.createElement('div'),
-      //listItemCreatedFunction: this.onListItemCreated
+      listItemCreatedFunction: this.onListItemCreated
     });
-    //layerList.on('trigger-action', (e) => this.onActionClicked(e));
+    layerList.on('trigger-action', (e) => this.onActionClicked(e));
 
     const layerListExpand = new EsriModules.Expand({
       view: this.mapView,
@@ -55,17 +54,13 @@ export class EsriLayerService {
         className: 'esri-icon-layers',
         id: 'show-shading'
       });
-      const sections: esriActionSections = new EsriModules.Collection();
-      const inner: esriActions = new EsriModules.Collection();
-      inner.add(action);
-      sections.add(inner);
-      listItem.actionsSections = sections;
+      listItem.actionsSections = <any>[[action]]; // have to any-fy this object to make TS happy
     }
   }
 
   private onActionClicked(event: any) : void {
     const id: string = event.action.id;
-    const currentLayer: __esri.Layer = event.item.layer;
+    const currentLayer: __esri.FeatureLayer = event.item.layer;
     console.log(event);
     if (id === 'show-shading') {
       console.log(`clicked action '${id}'`);
@@ -73,64 +68,69 @@ export class EsriLayerService {
     }
   }
 
-  private createRenderer(layer) {
-    // configure parameters for the color renderer generator
-    // the layer must be specified along with a field name
-    // or arcade expression. The basemap and other properties determine
-    // the appropriate default color scheme.
-    let slider = this.colorSlider;
-
+  private createRenderer(layer: __esri.FeatureLayer) : IPromise<void> {
+    this.showingDemoVars = !this.showingDemoVars;
+    if (this.currentRenderer != null) {
+      // swap using es6 syntax
+      [layer.renderer, this.currentRenderer] = [this.currentRenderer, layer.renderer];
+      [layer.opacity, this.currentOpacity] = [this.currentOpacity, layer.opacity];
+      return null;
+    }
     const colorParams = {
       layer: layer,
       basemap: this.mapView.map.basemap,
       field: 'CL0C00',
-      theme: 'high-to-low'
+      theme: 'high-to-low',
+      foo: 123
     };
 
     return EsriModules.colorRendererCreator.createContinuousRenderer(colorParams)
-      .then(function (response) {
-
+      .then( (response: __esri.ContinuousRendererResult) => {
         // set the renderer to the layer and add it to the map
+        this.currentRenderer = layer.renderer;
         layer.renderer = response.renderer;
+        this.currentOpacity = layer.opacity;
+        layer.opacity = 0.65;
 
         const histogramParams = {
           layer: colorParams.layer,
           field: colorParams.field,
-          numBins: 30
+          numBins: 30,
+          foo: 'bar'
         };
         return EsriModules.histogram(histogramParams)
-          .then(function (histogram) {
+          .then((histogram) => {
 
             // add the statistics and color visual variable objects
             // to the color slider parameters
-            const sliderParams = {
+            const sliderParams: __esri.ColorSliderProperties = {
               numHandles: 2,
               syncedHandles: false,
               statistics: response.statistics,
               visualVariable: response.visualVariable,
               histogram: histogram,
               minValue: response.statistics.min,
-              maxValue: response.statistics.max,
-              container: undefined
+              maxValue: response.statistics.max
             };
 
-            if (!slider) {
+            if (!this.colorSlider) {
               sliderParams.container = 'slider';
-              slider = new EsriModules.ColorSlider(sliderParams);
+              this.colorSlider = new EsriModules.ColorSlider(sliderParams);
               // when the user slides the handle(s), update the renderer
               // with the updated color visual variable object
-              slider.on('data-change', function() {
-                const renderer = layer.renderer.clone();
-                renderer.visualVariables = [EsriModules.lang.clone(slider.visualVariable)];
-                layer.renderer = renderer;
+              this.colorSlider.on('data-change', () => {
+                if (layer.renderer instanceof __esri.ClassBreaksRenderer) {
+                  const renderer = layer.renderer.clone();
+                  renderer.visualVariables = [EsriModules.lang.clone(this.colorSlider.visualVariable)];
+                  layer.renderer = renderer;
+                }
               });
             } else {
-              slider.set(sliderParams);
+              this.colorSlider.set(sliderParams);
             }
-            layer.opacity = 0.25;
           });
       })
-      .otherwise(function (err) {
+      .otherwise(err => {
         console.log('there was an error: ', err);
       });
   }
