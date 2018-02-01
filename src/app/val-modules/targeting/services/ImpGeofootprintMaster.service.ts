0f6864eaef5c3c66bce66a64627610d3b24cf414
David Gegenheimer <davidgeg@gmail.com>
2018-02-01 14:18:43 +0000

    US6582 - Generated Targeting domain data services

---
+++ src/app/val-modules/targeting/services/ImpGeofootprintMaster.service.ts
@@ -0,0 +1,32 @@
+/** A TARGETING domain data service representing the table: IMPOWER.IMP_GEOFOOTPRINT_MASTER
+ **
+ ** This class contains code operates against data in its data store.
+ ** See the contents of val-modules/common/services/datastore.service.ts to see built in
+ ** methods that all data services have.
+ **
+ ** NOTE: This class is generated only once and may be updated by developers.
+ **
+ ** ImpGeofootprintMaster.service.ts generated from VAL_ENTITY_GEN - v2.0
+ **/
+
+import { ImpGeofootprintMaster } from '../models/ImpGeofootprintMaster';
+import { RestDataService } from './../../common/services/restdata.service';
+import { DataStore } from '../../common/services/datastore.service';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ImpRadLookup } from '../models/ImpRadLookup';
+
+const dataUrl = 'v1/targeting/base/impgeofootprintmaster/search?q=impGeofootprintMaster';
+
+@Injectable()
+export class ImpGeofootprintMasterService extends DataStore<ImpGeofootprintMaster>
+{
+   constructor(private restDataService: RestDataService) {super(restDataService, dataUrl); }
+
+   private handleError(error: Response)
+   {
+      const errorMsg = `Status code: ${error.status} on url ${error.url}`;
+      console.error(errorMsg);
+      return Observable.throw(errorMsg);
+   }
+}
---
+++ src/app/val-modules/targeting/services/ImpGeofootprintTradeArea.service.ts
@@ -0,0 +1,32 @@
+/** A TARGETING domain data service representing the table: IMPOWER.IMP_GEOFOOTPRINT_TRADE_AREAS
+ **
+ ** This class contains code operates against data in its data store.
+ ** See the contents of val-modules/common/services/datastore.service.ts to see built in
+ ** methods that all data services have.
+ **
+ ** NOTE: This class is generated only once and may be updated by developers.
+ **
+ ** ImpGeofootprintTradeArea.service.ts generated from VAL_ENTITY_GEN - v2.0
+ **/
+
+import { ImpGeofootprintTradeArea } from '../models/ImpGeofootprintTradeArea';
+import { RestDataService } from './../../common/services/restdata.service';
+import { DataStore } from '../../common/services/datastore.service';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ImpRadLookup } from '../models/ImpRadLookup';
+
+const dataUrl = 'v1/targeting/base/impgeofootprinttradearea/search?q=impGeofootprintTradeArea';
+
+@Injectable()
+export class ImpGeofootprintTradeAreaService extends DataStore<ImpGeofootprintTradeArea>
+{
+   constructor(private restDataService: RestDataService) {super(restDataService, dataUrl); }
+
+   private handleError(error: Response)
+   {
+      const errorMsg = `Status code: ${error.status} on url ${error.url}`;
+      console.error(errorMsg);
+      return Observable.throw(errorMsg);
+   }
+}
