0ff4ae7d898017f008d6cc67b37815ed9d3388e4
David Gegenheimer <davidgeg@gmail.com>
2018-02-01 14:38:40 +0000

    US6582 - Generated media planning domain data services

---
+++ src/app/val-modules/mediaplanning/services/ImpProductAllocation.service.ts
@@ -0,0 +1,31 @@
+/** A MEDIAPLANNING domain data service representing the table: IMPOWER.IMP_PRODUCT_ALLOCATIONS
+ **
+ ** This class contains code operates against data in its data store.
+ ** See the contents of val-modules/common/services/datastore.service.ts to see built in
+ ** methods that all data services have.
+ **
+ ** NOTE: This class is generated only once and may be updated by developers.
+ **
+ ** ImpProductAllocation.service.ts generated from VAL_ENTITY_GEN - v2.0
+ **/
+
+import { ImpProductAllocation } from '../models/ImpProductAllocation';
+import { RestDataService } from './../../common/services/restdata.service';
+import { DataStore } from '../../common/services/datastore.service';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+const dataUrl = 'v1/mediaplanning/base/impproductallocation/search?q=impProductAllocation';
+
+@Injectable()
+export class ImpProductAllocationService extends DataStore<ImpProductAllocation>
+{
+   constructor(private restDataService: RestDataService) {super(restDataService, dataUrl); }
+
+   private handleError(error: Response)
+   {
+      const errorMsg = `Status code: ${error.status} on url ${error.url}`;
+      console.error(errorMsg);
+      return Observable.throw(errorMsg);
+   }
+}
