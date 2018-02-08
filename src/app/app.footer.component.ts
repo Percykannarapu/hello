395115ecf5db681c4e815f9c65512afa95b5c441
Aaron Cirillo <amcirillo@valassis.com>
2018-02-08 10:05:28 -0500

    US6288: got rid of fixed positioning on footer

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-footer',
     template: `
-        <div class="footer" style="bottom: 0%; position: fixed; width: 97.55%">
+        <div class="footer">
             <div class="card clearfix">
                 <span class="footer-text-left">Copyright Valassis, 2017</span>
                 <span class="footer-text-right"><span class="ui-icon ui-icon-copyright"></span>  <span>All Rights Reserved</span></span>
