4442705fdff5e532f62ef145822b320f3a99d893
Aaron Cirillo <amcirillo@valassis.com>
2018-02-16 00:02:34 -0500

    US6573: loading all layers by default broke geo selection, loading only zip by default for now until we figure it out

--- src/app/components/esri-layer-select/esri-layer-select.component.ts
+++ src/app/components/esri-layer-select/esri-layer-select.component.ts
@@ -51,7 +51,8 @@ export class EsriLayerSelectComponent implements OnInit, AfterViewInit {
       console.log ('selectedAnalysisLevels = ' + this.selectedAnalysisLevels);
 
       // set default layers and disable them
-      this.selectedAnalysisLevels = ['DMA', 'WRAP', 'DIG_ATZ', 'ATZ', 'ZIP'];
+      // this.selectedAnalysisLevels = ['DMA', 'WRAP', 'DIG_ATZ', 'ATZ', 'ZIP'];
+      this.selectedAnalysisLevels = ['ZIP'];
       this.mapService.setMapLayers(this.selectedAnalysisLevels);
       this.mapService.hideMapLayers();
     }
