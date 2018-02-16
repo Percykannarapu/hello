9840d48d3d8c7005808a51e7f3ad4c9a9bb7b2a5
kirk-clawson <kirk.clawson@outlook.com>
2018-02-16 11:06:59 -0500

    US6284 - environment.ts setup

---
+++ src/environments/environment.agol.ts
@@ -0,0 +1,51 @@
+export const environment = {
+  production: false
+};
+
+export class EnvironmentData {
+  public static fuseBaseUrl = 'https://servicesdev.valassislab.com/';
+  public static impowerBaseUrl = 'https://impowerdev.val.vlss.local/';
+
+  public static esri = {
+    portalUrl:  undefined
+  };
+  public static esriIdentityAuth = {
+    generatorUrl: `https://www.arcgis.com/sharing/generateToken`,
+    tokenServerUrl: `https://www.arcgis.com`,
+    userName: 'amcirillo_vlab2',
+    password: 'Password1'
+  };
+  public static oAuth = {
+    registerUrl: `${EnvironmentData.impowerBaseUrl}oauth/register`,
+    tokenUrl: `${EnvironmentData.impowerBaseUrl}oauth/token`
+  };
+
+  public static layerIds = {
+    dma: {
+      boundaries: '9205b77cd8c74773aefad268b6705543', // DMA_Boundaries
+      counties: undefined  // Counties
+    },
+    zip: {
+      topVars: '5742f3faba51493ab29f9e78bc5598d4', // ZIP Top Vars
+      centroids: '38b352fed65640beb0c246b82a24c881'  // ZIP_Centroids
+    },
+    atz: {
+      topVars: 'd3bf2b2a2a0a46f5bf10e8c6270767da', // ATZ_Top_Vars
+      centroids: '6053fb9b971245a6a61c3c20a2495732' // ATZ_Centroids
+    },
+    digital_atz: {
+      digitalTopVars: '2393d7bb2ac547c4a6bfa3d16f8febaa', // DIG_ATZ_Top_Vars
+      digitalCentroids: '2acb6088bfbb4be1abd502ea3b20ecf6'  // DIG_ATZ_Centroids
+    },
+    pcr: {
+      pcr: undefined
+    },
+    wrap: {
+      topVars: '09e5cdab538b43a4a6bd9a0d54b682a7'  // WRAP_Top_Vars
+    },
+    hh: {
+      vt: '837f4f8be375464a8971c56a0856198e', // vt layer
+      source: '5a99095bc95b45a7a830c9e25a389712'  // source featurelayer
+    }
+  };
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,3 +6,51 @@
 export const environment = {
   production: false
 };
+
+export class EnvironmentData {
+  public static fuseBaseUrl = 'https://servicesdev.valassislab.com/';
+  public static impowerBaseUrl = 'https://impowerdev.val.vlss.local/';
+
+  public static esri = {
+    portalUrl:  'https://vallomimpor1vm.val.vlss.local/arcgis/'
+  };
+  public static esriIdentityAuth = {
+    generatorUrl: `${EnvironmentData.esri.portalUrl}sharing/rest/generateToken`,
+    tokenServerUrl: `${EnvironmentData.esri.portalUrl}sharing/rest/portals`,
+    userName: 'admin',
+    password: 'password'
+  };
+  public static oAuth = {
+    registerUrl: `${EnvironmentData.impowerBaseUrl}oauth/register`,
+    tokenUrl: `${EnvironmentData.impowerBaseUrl}oauth/token`
+  };
+
+  public static layerIds = {
+    dma: {
+      boundaries: '5c8d7e4a824f4aa0b254925348f2a14a', // DMA_Boundaries
+      counties: '39b51d9d498f4107bc69ac30f31ac115'  // Counties
+    },
+    zip: {
+      topVars: '23a54308e914496aa24d94a9b36776a0', // ZIP Top Vars
+      centroids: '88120ac630d746239b133296e87b8e1f'  // ZIP_Centroids
+    },
+    atz: {
+      topVars: 'c0ee701ee95f4bbdbc15ded2a37ca802', // ATZ_Top_Vars
+      centroids: 'fd4b078fc2424dd5a48af860dc421431' // ATZ_Centroids
+    },
+    digital_atz: {
+      digitalTopVars: 'a4449b3ee55442af881f6ac660ca8163', // DIG_ATZ_Top_Vars
+      digitalCentroids: '377018a24ba14afa9e02e56110b3a568'  // DIG_ATZ_Centroids
+    },
+    pcr: {
+      pcr: '8fee65ffdb784c67b76c356e9c26605f'
+    },
+    wrap: {
+      topVars: 'cde7f5d605bf48c4a74b0cd1a47ceccb'  // WRAP_Top_Vars
+    },
+    hh: {
+      vt: undefined, // vt layer
+      source: undefined  // source featurelayer
+    }
+  };
+}
