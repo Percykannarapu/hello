db124bf27745865ab7d14f35eccf31b46f0c0273
Aaron Cirillo <amcirillo@valassis.com>
2018-02-22 12:20:03 -0500

    US6257: corrected some product codes in discovery component to work correctly with RAD

--- src/app/components/discovery-input/discovery-input.component.ts
+++ src/app/components/discovery-input/discovery-input.component.ts
@@ -64,14 +64,14 @@ export class DiscoveryInputComponent implements OnInit
       //this.impDiscoveryService.analysisLevel.subscribe(data => this.onAnalysisSelectType(data));
 
       this.products = [
-         {productName: 'Display Advertising',         productCode: 'DISPLAY'},
-         {productName: 'Email',                       productCode: 'EMAIL'},
-         {productName: 'Insert - Newspaper',          productCode: 'INS_NEWS'},
-         {productName: 'Insert - Shared Mail',        productCode: 'INS_SHARED'},
-         {productName: 'RedPlum Plus Dynamic Mobile', productCode: 'RPDM'},
+         {productName: 'Display Advertising',         productCode: 'N/A'},
+         {productName: 'Email',                       productCode: 'N/A'},
+         {productName: 'Insert - Newspaper',          productCode: 'NP Insert'},
+         {productName: 'Insert - Shared Mail',        productCode: 'SM Insert'},
+         {productName: 'RedPlum Plus Dynamic Mobile', productCode: 'N/A'},
          {productName: 'Variable Data Postcard',      productCode: 'VDP'},
-         {productName: 'VDP + Email',                 productCode: 'VDP_EMAIL'},
-         {productName: 'Red Plum Wrap',               productCode: 'WRAP'}
+         {productName: 'VDP + Email',                 productCode: 'N/A'},
+         {productName: 'Red Plum Wrap',               productCode: 'SM Wrap'}
       ];
 
       this.categories = [
