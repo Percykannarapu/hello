81cab67661c6ee7766e24409ba8ca108c46d9696
kirk-clawson <kirk.clawson@outlook.com>
2018-03-22 08:34:29 -0400

    Cleanup: fixing lint errors and name consistency issues

--- src/app/components/upload-locations/upload-locations.component.ts
+++ src/app/components/upload-locations/upload-locations.component.ts
@@ -7,12 +7,10 @@ import { ValGeocodingRequest } from '../../models/val-geocoding-request.model';
 import { ValGeocodingResponse } from '../../models/val-geocoding-response.model';
 import { FileUpload } from 'primeng/primeng';
 import { ValSiteListService } from '../../services/app-site-list.service';
-import { ValMapService } from '../../services/app-map.service';
-import * as XLSX from 'xlsx';
 import { ImpDiscoveryUI } from '../../models/ImpDiscoveryUI';
 import { ImpDiscoveryService } from '../../services/ImpDiscoveryUI.service';
 
-
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'val-upload-locations',
@@ -46,32 +44,34 @@ export class UploadLocationsComponent {
     });
   }
 
-  public uploadCsv(event: any) : void {
+  public uploadFile(event: any) : void {
     const discoveryUI: ImpDiscoveryUI[] = this.impDiscoveryService.get();
-   if (discoveryUI[0].analysisLevel !== '' && discoveryUI[0].analysisLevel != null){
-    console.log('Upload Clicked');
-    const reader = new FileReader();
-    const name: String = event.files[0].name;
-    console.log('file Name:::', name);
-    if (name.includes('.xlsx') || name.includes('.xls') ){
-        this.parseExcelFile(event);
-    }
-    else {
-      reader.readAsText(event.files[0]);
-      reader.onload = () => {
-        this.onFileUpload(reader.result);
-      };
-    }
-    
-    this.fileUploadEl.clear();
-    // workaround for https://github.com/primefaces/primeng/issues/4816
-    this.fileUploadEl.basicFileInput.nativeElement.value = '';
-  } else{
-    this.messageService.add({ severity: 'error', summary: 'Draw Buffer Error', detail: `You must select an Analysis Level on the Discovery tab before adding Sites`});
+    if (discoveryUI[0].analysisLevel !== '' && discoveryUI[0].analysisLevel != null) {
+      console.log('Upload Clicked');
+      const reader = new FileReader();
+      const name: String = event.files[0].name;
+      console.log('file Name:::', name);
+      if (name.includes('.xlsx') || name.includes('.xls') ) {
+        reader.readAsBinaryString(event.files[0]);
+        reader.onload = () => {
+          this.parseExcelFile(reader.result);
+        };
+      } else {
+        reader.readAsText(event.files[0]);
+        reader.onload = () => {
+          this.parseCsvFile(reader.result);
+        };
+      }
+
+      this.fileUploadEl.clear();
+      // workaround for https://github.com/primefaces/primeng/issues/4816
+      this.fileUploadEl.basicFileInput.nativeElement.value = '';
+    } else {
+      this.messageService.add({ severity: 'error', summary: 'Draw Buffer Error', detail: `You must select an Analysis Level on the Discovery tab before adding Sites`});
   }
 }
 
-  private onFileUpload(dataBuffer: string) {
+  private parseCsvFile(dataBuffer: string) {
     const rows: string[] = dataBuffer.split(/\r\n|\n/);
     const header: string = rows.shift();
     try {
@@ -91,32 +91,22 @@ export class UploadLocationsComponent {
     }
   }
 
+  // to process excel upload data
+  public parseExcelFile(bstr: string) : void {
+    console.log('process excel data::');
+    try {
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+      const worksheetName: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[worksheetName];
+      const csvData  = XLSX.utils.sheet_to_csv(ws);
+      this.parseCsvFile(csvData);
+    } catch (e) {
+      this.handleError(`${e}`);
+    }
+  }
+
   private handleError(message: string) : void {
     this.displayGcSpinner = false;
     this.messageService.add({ severity: 'error', summary: 'Geocoding Error', detail: message });
-  }
-
-  // to process excel upload data
-  public parseExcelFile(event: any ) : void {
-    console.log('process excel data::');
-  
-    const target: DataTransfer = <DataTransfer>(event);
-    const reader: FileReader = new FileReader();
-    reader.readAsBinaryString(target.files[0]);
-    reader.onload = () => {
-      const bstr: string = reader.result;
-      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-      const wsname: string = wb.SheetNames[0];
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-      const csvData  = XLSX.utils.sheet_to_csv(ws);
-      //console.log('csv data:::data1', csvData);
-      this.onFileUpload(csvData);
-      this.fileUploadEl.clear();
-      // workaround for https://github.com/primefaces/primeng/issues/4816
-      this.fileUploadEl.basicFileInput.nativeElement.value = '';
-
-    };
-    
-
   }
 }
