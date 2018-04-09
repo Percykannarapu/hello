e87ada0e8fee68924f4fc022e07d69cdafac53ad
kirk-clawson <kirk.clawson@outlook.com>
2018-04-09 12:25:15 -0400

    DE1708: Add growl message when trying to apply trade area radius without an Analysis Level

--- src/app/components/tradearea-define/tradearea-define.component.ts
+++ src/app/components/tradearea-define/tradearea-define.component.ts
@@ -1,10 +1,13 @@
 import { SelectItem } from 'primeng/primeng';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppConfig } from '../../app.config';
 import { TradeAreaUIModel } from './trade-area-ui.model';
 import { RadialTradeAreaDefaults, ValTradeAreaService } from '../../services/app-trade-area.service';
 import { ImpGeofootprintLocationService } from '../../val-modules/targeting/services/ImpGeofootprintLocation.service';
-import { MessageService } from 'primeng/components/common/messageservice';
+import { Subscription } from 'rxjs/Subscription';
+import { ImpDiscoveryService } from '../../services/ImpDiscoveryUI.service';
+import { AppMessagingService } from '../../services/app-messaging.service';
+import { map } from 'rxjs/operators';
 
 type SiteType = 'Site' | 'Competitor';
 interface MergeType { value: string; }
@@ -14,19 +17,27 @@ interface MergeType { value: string; }
     templateUrl: './tradearea-define.component.html',
     styleUrls: ['./tradearea-define.component.css']
 })
-export class TradeareaDefineComponent {
+export class TradeareaDefineComponent implements OnInit, OnDestroy {
 
-  private siteTradeAreas: TradeAreaUIModel[];
-  private siteMergeType: MergeType;
-  private competitorTradeAreas: TradeAreaUIModel[];
-  private competitorMergeType: MergeType;
+  private readonly siteTradeAreas: TradeAreaUIModel[];
+  private readonly siteMergeType: MergeType;
+  private readonly competitorTradeAreas: TradeAreaUIModel[];
+  private readonly competitorMergeType: MergeType;
+
+  private analysisLevelSub: Subscription;
+  private siteCountSub: Subscription;
+  private competitorCountSub: Subscription;
+  private currentAnalysisLevel: string;
+  private locationCountMap: Map<string, number> = new Map<string, number>();
 
   currentTradeAreas: TradeAreaUIModel[];
   currentMergeType: MergeType;
   currentSiteType: SiteType;
   tradeAreaMergeTypes: SelectItem[];
 
-  constructor(private tradeAreaService: ValTradeAreaService, private config: AppConfig, private impGeofootprintLocationService: ImpGeofootprintLocationService, private messageService: MessageService) {
+  constructor(private tradeAreaService: ValTradeAreaService, private config: AppConfig,
+              private locationService: ImpGeofootprintLocationService, private messageService: AppMessagingService,
+              private discoveryService: ImpDiscoveryService) {
     this.tradeAreaMergeTypes = [
       { label: 'No Merge', value: 'No Merge' },
       { label: 'Merge Each', value: 'Merge Each' },
@@ -49,10 +60,33 @@ export class TradeareaDefineComponent {
     this.currentMergeType = this.siteMergeType;
   }
 
+  public ngOnInit() : void {
+    this.siteCountSub = this.locationService.storeObservable.pipe(
+      map(locations => locations.filter(loc => loc.clientLocationTypeCode === 'Site').length)
+    ).subscribe(siteCount => this.locationCountMap.set('Site', siteCount));
+    this.competitorCountSub = this.locationService.storeObservable.pipe(
+      map(locations => locations.filter(loc => loc.clientLocationTypeCode === 'Competitor').length)
+    ).subscribe(competitorCount => this.locationCountMap.set('Competitor', competitorCount));
+    this.analysisLevelSub = this.discoveryService.storeObservable.pipe(
+      map(disco => disco && disco.length > 0 ? disco[0].analysisLevel : null)
+    ).subscribe(al => this.currentAnalysisLevel = al);
+  }
+
+  public ngOnDestroy() : void {
+    if (this.analysisLevelSub) this.analysisLevelSub.unsubscribe();
+  }
+
   public onApplyBtnClick() {
-    if (this.impGeofootprintLocationService.get().length < 1){
-      this.messageService.add({ severity: 'error', summary: 'Draw Buffer Error', detail: `You must add at least 1 Site before attempting to apply a trade area to Sites`});
-    } else {
+    let isValid = true;
+    if (!this.locationCountMap.has(this.currentSiteType) || this.locationCountMap.get(this.currentSiteType) < 1) {
+      this.messageService.showGrowlError('Trade Area Error', `You must add at least 1 ${this.currentSiteType} before applying a trade area to ${this.currentSiteType}s`);
+      isValid = false;
+    }
+    if (isValid && (this.currentAnalysisLevel == null || this.currentAnalysisLevel === '') && this.currentSiteType === 'Site') {
+      this.messageService.showGrowlError('Trade Area Error', `You must select an Analysis Level before applying a trade area to Sites`);
+      isValid = false;
+    }
+    if (isValid) {
       const tradeAreas: TradeAreaUIModel[] = this.currentTradeAreas.filter(ta => ta.isShowing) || [];
       const settings = new RadialTradeAreaDefaults(tradeAreas.map(ta => ta.tradeArea), this.currentMergeType.value);
       this.tradeAreaService.applyRadialDefaults(settings, this.currentSiteType);
@@ -72,6 +106,6 @@ export class TradeareaDefineComponent {
   }
 
   applyDisabled() : boolean {
-    return this.currentTradeAreas.some(t => t.isValid === false) || this.currentTradeAreas.every(t => t.isValid == null) ;
+    return this.currentTradeAreas.some(t => t.isValid === false) || this.currentTradeAreas.every(t => t.isValid == null);
   }
 }
