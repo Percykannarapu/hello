247325c1741ac1e2af660fa1a4b402d667814ac7
kirk-clawson <kirk.clawson@outlook.com>
2018-05-24 12:45:14 -0400

    US7418 - Hide Online audience controls when analysis level is PCR

--- src/app/components/target-audience/target-audience.component.ts
+++ src/app/components/target-audience/target-audience.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TargetAudienceService } from '../../services/target-audience.service';
 import { Observable } from 'rxjs';
 import { AudienceDataDefinition } from '../../models/audience-data.model';
+import { distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { ImpDiscoveryService } from '../../services/ImpDiscoveryUI.service';
 
 @Component({
   selector: 'val-target-audience',
@@ -10,11 +12,17 @@ import { AudienceDataDefinition } from '../../models/audience-data.model';
 })
 export class TargetAudienceComponent implements OnInit {
   public audiences$: Observable<AudienceDataDefinition[]>;
+  public analysisLevel$: Observable<string>;
 
-  constructor(private audienceService: TargetAudienceService) { }
+  constructor(private audienceService: TargetAudienceService, private discoveryService: ImpDiscoveryService) { }
 
   ngOnInit() {
     this.audiences$ = this.audienceService.audiences$;
+    this.analysisLevel$ = this.discoveryService.storeObservable.pipe(
+      filter(disc => disc != null && disc.length > 0 && disc[0].analysisLevel != null && disc[0].analysisLevel !== ''),
+      map(disc => disc[0].analysisLevel),
+      distinctUntilChanged()
+    );
   }
 
 }
