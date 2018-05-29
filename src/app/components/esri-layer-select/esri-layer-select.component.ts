15ded6adf377c4e0a3d3a04096b9c6ed4f42c2af
Singla <SinglaM@valassis.com>
2018-05-29 12:16:12 -0400

    US6818: Zoom to Entire Trade Area

--- src/app/components/esri-layer-select/esri-layer-select.component.ts
+++ src/app/components/esri-layer-select/esri-layer-select.component.ts
@@ -9,6 +9,12 @@ import { ImpDiscoveryService } from '../../services/ImpDiscoveryUI.service';
 import { ImpGeofootprintGeoAttribService } from '../../val-modules/targeting/services/ImpGeofootprintGeoAttribService';
 import { ValGeoService } from '../../services/app-geo.service';
 import { ImpGeofootprintTradeAreaService } from '../../val-modules/targeting/services/ImpGeofootprintTradeArea.service';
+import { ImpGeofootprintLocation } from '../../val-modules/targeting/models/ImpGeofootprintLocation';
+import { calculateStatistics, toUniversalCoordinates } from '../../app.utils';
+import { EsriLayerService } from '../../esri-modules/layers/esri-layer.service';
+import { EsriQueryService } from '../../esri-modules/layers/esri-query.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'val-esri-layer-select',
@@ -17,9 +23,11 @@ import { ImpGeofootprintTradeAreaService } from '../../val-modules/targeting/ser
 })
 export class EsriLayerSelectComponent implements OnInit, AfterViewInit {
 
-
   public selectedAnalysisLevels: string[] = [];
   public currentAnalysisLevel: string = '';
+  public currentPoints;
+  private geoSubscription: Subscription;
+  private geocodes: string[];
 
   constructor(public mapService: MapService,
               private impGeofootprintGeoService: ImpGeofootprintGeoService,
@@ -28,15 +36,24 @@ export class EsriLayerSelectComponent implements OnInit, AfterViewInit {
               public impDiscoveryService: ImpDiscoveryService,
               private attributeService: ImpGeofootprintGeoAttribService ,
               private valGeoService: ValGeoService,
-              private tradeAreaService: ImpGeofootprintTradeAreaService) {
+              private tradeAreaService: ImpGeofootprintTradeAreaService,
+              private esriLayerService: EsriLayerService,
+              private queryService: EsriQueryService,
+              private config: AppConfig) {
     }
 
   public ngOnInit() {
     this.impDiscoveryService.storeObservable.subscribe(d => {
       if (d && d[0] && d[0].analysisLevel) this.currentAnalysisLevel = d[0].analysisLevel;
     });
+    this.geoSubscription = this.valGeoService.uniqueSelectedGeocodes$.subscribe(geocodes => {
+      this.geocodes = geocodes;
+    });
+
+
   }
 
+ 
   public ngAfterViewInit() {
       this.modules.onReady(() => { this.init(); });
   }
@@ -54,6 +71,31 @@ export class EsriLayerSelectComponent implements OnInit, AfterViewInit {
       console.log(' fired Clear selections:::');
       this.impGeofootprintGeoService.clearAll();
       this.attributeService.clearAll();
+    }
+
+    public onZoomToTA() {
+       const latitudes: number[] = [];
+       const longitudes: number[] = [];
+       const layerId = this.config.getLayerIdForAnalysisLevel(this.currentAnalysisLevel, false);
+       const query$ = this.queryService.queryAttributeIn(layerId, 'geocode', this.geocodes, false, ['latitude', 'longitude']);
+       const sub = query$.subscribe(
+         selections => {
+           selections.forEach(g => {
+             if (g.attributes.latitude != null && !Number.isNaN(Number(g.attributes.latitude))) {
+               latitudes.push(Number(g.attributes.latitude));
+             }
+             if (g.attributes.longitude != null && !Number.isNaN(Number(g.attributes.longitude))) {
+              longitudes.push(Number(g.attributes.longitude));
+            }
+           });
+         },
+         err => { console.error('Error getting lats and longs from layer', err); },
+         () => {
+           const xStats = calculateStatistics(longitudes);
+           const yStats = calculateStatistics(latitudes);
+           this.esriMapService.zoomOnMap(xStats, yStats, this.geocodes.length);
+         }
+       );
     }
 
     onRevertToTradeArea(){
