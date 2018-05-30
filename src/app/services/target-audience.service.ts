2934b1ef86512ca2925d10c666c8fa3653dbcaad
Reddy <ReddyN@valassis.com>
2018-05-30 10:26:37 -0400

    US7676: offline audience usagemetric

--- src/app/services/target-audience.service.ts
+++ src/app/services/target-audience.service.ts
@@ -114,7 +114,7 @@ export class TargetAudienceService implements OnDestroy {
         () => {
           try {
             const usageMetricName: ImpMetricName = new ImpMetricName({ namespace: 'targeting', section: 'audience', target: 'online', action: 'export' });
-            const metricText = audiences[0].audienceIdentifier + '~' + audiences[0].audienceName + '~' + audiences[0].audienceSourceName + '' + this.discoveryService.get()[0].analysisLevel;
+            const metricText = audiences[0].audienceIdentifier + '~' + audiences[0].audienceName + '~' + audiences[0].audienceSourceName + '~' + this.discoveryService.get()[0].analysisLevel;
             this.usageService.createCounterMetric(usageMetricName, metricText, convertedData.length);
             const fmtDate: string = new Date().toISOString().replace(/\D/g, '').slice(0, 13);
             const fileName = `NatlExtract_${this.currentAnalysisLevel}_${audiences[0].audienceIdentifier}_${fmtDate}.xlsx`;
