e70e8a6e2c35cc324e18a19ff86ffe0ef74aa7e5
kannarapup <kannarapup@valassis.com>
2018-05-30 16:30:53 -0400

    US6348: updated conditions to tag CENTROID

--- src/app/services/app-geocoding.service.ts
+++ src/app/services/app-geocoding.service.ts
@@ -64,7 +64,7 @@ export class ValGeocodingService {
               if (d['Match Quality'] === 'E' || (d['Match Code'].startsWith('E') && !d['Match Quality'].startsWith('Z'))) {
                 d['Geocode Status'] = 'ERROR';
                 fail.push(new ValGeocodingResponse(d));
-              } else if (d['Match Quality'] === '' || (d['Match Quality'].startsWith('Z') && !d['Match Quality'].startsWith('ZT9')) || d['Match Code'] === 'Z') {
+              } else if ((d['Match Quality'].startsWith('Z') && !d['Match Quality'].startsWith('ZT9')) || d['Match Code'] === 'Z') {
                 d['Geocode Status'] = 'CENTROID';
                 fail.push(new ValGeocodingResponse(d));
               } else {
