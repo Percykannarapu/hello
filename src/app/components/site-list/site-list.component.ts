86dcb624f94e17677f52ce81b80f3b63fee9240d
Singla <SinglaM@valassis.com>
2018-06-08 10:47:12 -0400

    US7383 Populate Site Name with Site Number when no Site Name

--- src/app/components/site-list/site-list.component.ts
+++ src/app/components/site-list/site-list.component.ts
@@ -73,6 +73,12 @@ export class SiteListComponent implements OnInit {
       this.columnOptions.push({ label: column.header, value: column });
       this.selectedColumns.push(column);
     }
+    this.siteListService.allSites$.subscribe(locations => { 
+      locations.forEach(loc => { 
+      loc.location.locationName = (loc.location.locationName == null || loc.location.locationName === '') ? loc.location.locationNumber : loc.location.locationName; 
+      console.log('Location::: ', loc); 
+      }); 
+      } );
   }
 
   public onListTypeChange(data: 'Site' | 'Competitor') {
