9f640bcd96d055dc1aec0f3bd24e84654847442d
Reddy <ReddyN@valassis.com>
2018-06-22 15:18:24 -0400

    US7824: task TA9625 verify metric events

--- src/app/services/ImpDiscoveryUI.service.ts
+++ src/app/services/ImpDiscoveryUI.service.ts
@@ -14,6 +14,8 @@ import { AppMessagingService } from './app-messaging.service';
 import { ImpGeofootprintLocationService } from '../val-modules/targeting/services/ImpGeofootprintLocation.service';
 import { UsageService } from './usage.service';
 import { isNumber } from '../app.utils';
+import { AppStateService } from './app-state.service';
+import { ImpProject } from '../val-modules/targeting/models/ImpProject';
 
 const dataUrl = '';
 
@@ -29,7 +31,7 @@ export class ImpDiscoveryService // extends DataStore<ImpDiscoveryUI>
       public storeProjectTrackerData;
       public needsGeoRefresh: boolean = false;
 
-      constructor(private restDataService: RestDataService) {
+      constructor(private restDataService: RestDataService, private appStateService: AppStateService) {
             // super(restDataService, dataUrl, null, 'ImpDiscovery');
       }
 
@@ -45,74 +47,75 @@ export class ImpDiscoveryService // extends DataStore<ImpDiscoveryUI>
       }
 
       public discoveryUsageMetricsCreate(actionName: string) : CounterMetrics[] {
-        return;
+        const impProject: ImpProject = this.appStateService.currentProject$.getValue();
+       // return;
         //let discoverUIData: ImpDiscoveryUI = this.get()[0];
-        // const counterMetrics = [];
-        // let usageMetricName = null;
-        //
-        // if (discoverUIData.productCode != null || discoverUIData.productCode != '') {
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'product', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, discoverUIData.productCode, null));
-        // }
-        // if (discoverUIData.industryCategoryName != null || discoverUIData.industryCategoryName != '') {
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'category', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, discoverUIData.industryCategoryName, null));
-        // }
-        // if (discoverUIData.analysisLevel) {
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'analysis-level', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, discoverUIData.analysisLevel, null));
-        // }
-        // if (discoverUIData.cpm != null) {
-        //   const blendedCpm = discoverUIData.cpm != null ? discoverUIData.cpm : null;
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'blended-cpm', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, null, blendedCpm));
-        // }
-        // if (discoverUIData.valassisCPM != null) {
-        //   const valassisCpm = discoverUIData.valassisCPM != null ? discoverUIData.valassisCPM : null;
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'valassis-cpm', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, null, valassisCpm));
-        // }
-        // if (discoverUIData.anneCPM != null) {
-        //   const anneCPM = discoverUIData.anneCPM != null ? discoverUIData.anneCPM : null;
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'anne-cpm', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, null, anneCPM));
-        // }
-        // if (discoverUIData.soloCPM != null) {
-        //   const soloCpm = discoverUIData.soloCPM != null ? discoverUIData.soloCPM : null;
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'solo-cpm', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, null, soloCpm));
-        // }
-        // if (discoverUIData.totalBudget != null) {
-        //   const totalBudget = discoverUIData.totalBudget != null ? discoverUIData.totalBudget : null;
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'dollar-budget', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, null, totalBudget));
-        // }
-        // if (discoverUIData.circBudget != null) {
-        //   const circBudget = discoverUIData.circBudget != null ? discoverUIData.circBudget : null;
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'circ-budget', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, null, circBudget));
-        // }
-        // if (discoverUIData.selectedSeason != null) {
-        //   usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'season', action: actionName });
-        //   counterMetrics.push(new CounterMetrics(usageMetricName, discoverUIData.selectedSeason, null));
-        // }
-        // usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-pob-geo', action: actionName });
-        // const ispob = discoverUIData.includePob ? 1 : 0;
-        // counterMetrics.push(new CounterMetrics(usageMetricName, ispob.toString(), null));
-        //
-        // usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-valassis-geo', action: actionName });
-        // const isvalGeo = discoverUIData.includeValassis ? 1 : 0;
-        // counterMetrics.push(new CounterMetrics(usageMetricName, isvalGeo.toString(), null));
-        //
-        // usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-anne-geo', action: actionName });
-        // const isAnneGeo = discoverUIData.includeAnne ? 1 : 0;
-        // counterMetrics.push(new CounterMetrics(usageMetricName, isAnneGeo.toString(), null));
-        //
-        // usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-solo-geo', action: actionName });
-        // const isSoloGeo = discoverUIData.includeAnne ? 1 : 0;
-        // counterMetrics.push(new CounterMetrics(usageMetricName, isSoloGeo.toString(), null));
-        //
-        // return counterMetrics;
+        const counterMetrics = [];
+        let usageMetricName = null;
+        
+        if (impProject.radProduct != null || impProject.radProduct != '') {
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'product', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, impProject.radProduct, null));
+        }
+        if (impProject.industryCategoryCode != null || impProject.industryCategoryCode != '') {
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'category', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, impProject.industryCategoryCode, null));
+        }
+        if (impProject.methAnalysis != null) {
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'analysis-level', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, impProject.methAnalysis, null));
+        }
+        if (impProject.estimatedBlendedCpm != null) {
+          const blendedCpm = impProject.estimatedBlendedCpm != null ? impProject.estimatedBlendedCpm : null;
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'blended-cpm', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, null, blendedCpm));
+        }
+        if (impProject.smValassisCpm != null) {
+          const valassisCpm = impProject.smValassisCpm != null ? impProject.smValassisCpm : null;
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'valassis-cpm', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, null, valassisCpm));
+        }
+        if (impProject.smAnneCpm != null) {
+          const anneCPM = impProject.smAnneCpm != null ? impProject.smAnneCpm : null;
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'anne-cpm', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, null, anneCPM));
+        }
+        if (impProject.smSoloCpm != null) {
+          const soloCpm = impProject.smSoloCpm != null ? impProject.smSoloCpm : null;
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'solo-cpm', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, null, soloCpm));
+        }
+        if (impProject.totalBudget != null) {
+          const totalBudget = impProject.totalBudget != null ? impProject.totalBudget : null;
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'dollar-budget', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, null, totalBudget));
+        }
+        if (impProject.isCircBudget) {
+          const circBudget = impProject.totalBudget != null ? impProject.totalBudget : null;
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'circ-budget', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, null, circBudget));
+        }
+        if (impProject.impGeofootprintMasters[0].methSeason != null) {
+          usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'season', action: actionName });
+          counterMetrics.push(new CounterMetrics(usageMetricName, impProject.impGeofootprintMasters[0].methSeason, null));
+        }
+        usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-pob-geo', action: actionName });
+        const ispob = impProject.isExcludePob ? 1 : 0;
+        counterMetrics.push(new CounterMetrics(usageMetricName, ispob.toString(), null));
+        
+        usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-valassis-geo', action: actionName });
+        const isvalGeo = impProject.isIncludeValassis ? 1 : 0;
+        counterMetrics.push(new CounterMetrics(usageMetricName, isvalGeo.toString(), null));
+        
+        usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-anne-geo', action: actionName });
+        const isAnneGeo = impProject.isIncludeAnne ? 1 : 0;
+        counterMetrics.push(new CounterMetrics(usageMetricName, isAnneGeo.toString(), null));
+        
+        usageMetricName = new ImpMetricName({ namespace: 'targeting', section: 'colorbox-input', target: 'include-solo-geo', action: actionName });
+        const isSoloGeo = impProject.isIncludeSolo ? 1 : 0;
+        counterMetrics.push(new CounterMetrics(usageMetricName, isSoloGeo.toString(), null));
+        
+        return counterMetrics;
       }
 
       private formatDate(date) : string {
