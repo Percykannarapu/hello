b60ecc90747bf6c3ba528547dc38fafd072dbefd
Reddy <ReddyN@valassis.com>
2018-08-22 08:21:43 -0400

    protractor code

--- src/app/services/app-trade-area.service.ts
+++ src/app/services/app-trade-area.service.ts
@@ -226,10 +226,10 @@ export class AppTradeAreaService {
     }
     console.log(`Drawing ${siteType} trade areas`, drawnTradeAreas);
     this.layerService.addToTradeAreaLayer(siteType, drawnTradeAreas, mergeType);
+    // reset the defaults that get applied to new locations
     const taValues: any[] = [];
     radii.forEach(radius => {
       taValues.push({radius: radius , selected: true });
-        //this.currentDefaults.set(siteType, {radius: 4 , selected: true });
     });
     this.currentDefaults.set(siteType, taValues);
   }
