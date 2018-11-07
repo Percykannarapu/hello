e6ad11f5f05ca100accad5ff3c5c51928adb7ffe
Reddy <ReddyN@valassis.com>
2018-11-07 16:41:36 -0500

    US7845:Checking and Unchecking Geos that cover multiple stores

--- src/app/components/geofootprint-geo-panel/geofootprint-geo-panel.component.ts
+++ src/app/components/geofootprint-geo-panel/geofootprint-geo-panel.component.ts
@@ -20,6 +20,7 @@ import { ConfirmationService } from 'primeng/primeng';
 import { ImpMetricName } from '../../val-modules/metrics/models/ImpMetricName';
 import { UsageService } from '../../services/usage.service';
 import { log } from 'util';
+import { reject } from 'q';
 
 export interface FlatGeo {
    fgId: number;
@@ -239,11 +240,26 @@ export class GeofootprintGeoPanelComponent implements OnInit {
          //geo.isActive = isSelected;
         //US7845: changes to filter checking/unchecking geos that cover multiple stores 
          const filteredtGeos = currentProject.getImpGeofootprintGeos().filter(dupGeo => geo.geocode === dupGeo.geocode);
-         filteredtGeos.forEach(dupGeo => dupGeo.isActive = isSelected);
-
-         //This change to update Datastore to fire toggleGeoSelection DE1933
-         this.impGeofootprintGeoService.makeDirty();
-         this.impGeofootprintGeoAttribService.makeDirty();
+         if (filteredtGeos.length > 0 && geo.isActive){
+             this.confirmationService.confirm({
+              message: 'You are about to unselect a Home Geo for at least one of the sites.  Ok to continue?',
+              header: ': Unselecting a Home Geo',
+              accept: () => {
+                filteredtGeos.forEach(dupGeo => dupGeo.isActive = isSelected);
+                 //This change to update Datastore to fire toggleGeoSelection DE1933
+                 this.impGeofootprintGeoService.makeDirty();
+                 this.impGeofootprintGeoAttribService.makeDirty();
+              },
+              reject: () => {
+                geo.isActive = isSelected;
+                 //This change to update Datastore to fire toggleGeoSelection DE1933
+                  this.impGeofootprintGeoService.makeDirty();
+                  this.impGeofootprintGeoAttribService.makeDirty();  
+              }
+             });
+         }else {
+          geo.isActive = isSelected;
+         }
 
          let metricText = null;
          const cpm = currentProject.estimatedBlendedCpm != null ? currentProject.estimatedBlendedCpm : 0;
