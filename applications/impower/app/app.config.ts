8fa9d6fe795f103a485e27a643244e087a0d0b5e
kirk-clawson <kirk.clawson@outlook.com>
2018-12-18 10:41:37 -0500

    Chore - Turn off aot and revert webgl issue until modularization / aot compat can be dealt with

--- applications/impower/app/app.config.ts
+++ applications/impower/app/app.config.ts
@@ -211,7 +211,8 @@ export class AppConfig implements LoggingConfiguration {
   }
 
   public webGLIsAvailable() : boolean {
-    return true;
+    console.log('Esri config inside app config: ', this.esriSettings);
+    return this.esriSettings.dojoConfig['has'] && (this.esriSettings.dojoConfig['has']['esri-featurelayer-webgl'] === 1);
   }
 
 }
