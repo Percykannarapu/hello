48a182a3c8caba4d09f4425df9a6ec98871661b0
Reddy <ReddyN@valassis.com>
2019-04-01 09:27:16 -0400

    US9174: clear distance TA fields

--- applications/impower/app/components/trade-area-tab/trade-area-tab.component.ts
+++ applications/impower/app/components/trade-area-tab/trade-area-tab.component.ts
@@ -103,7 +103,9 @@ export class TradeAreaTabComponent implements OnInit {
       this.currentLocationsCount$.next(l.length);
     });
 
-    this.hasSiteProvidedTradeAreas$ = this.stateService.hasSiteProvidedTradeAreas$.pipe(distinctUntilChanged());
+    this.stateService.hasSiteProvidedTradeAreas$.pipe(
+      map(aud => distinctUntilChanged() && this.validateTA())
+    );
     this.hasCompetitorProvidedTradeAreas$ = this.stateService.hasCompetitorProvidedTradeAreas$.pipe(distinctUntilChanged());
   }
 
@@ -213,5 +215,13 @@ export class TradeAreaTabComponent implements OnInit {
       this.store$.dispatch(new StopBusyIndicator({ key: 'AUDIENCETA' }));
    }
    );
+  }
+
+  public validateTA(){
+    const currentLocations = this.appLocationService.cachedTradeAreas.map(ta => ta.impGeofootprintLocation);
+    const isRadii = currentLocations.find(loc => loc.radius1 != null && loc.radius2 != null && loc.radius3 != null);
+    
+    
+    return isRadii != null ? true : false;
   }
 }
