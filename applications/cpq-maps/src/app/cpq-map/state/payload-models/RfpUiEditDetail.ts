459ec755cbfa8209e43957f5e31d0cbc193767e0
kannarapup <kannarapup@valassis.com>
2019-05-02 11:19:07 -0400

    US8800: updated field on RFPUiEditDetail

--- applications/cpq-maps/src/app/cpq-map/state/payload-models/RfpUiEditDetail.ts
+++ applications/cpq-maps/src/app/cpq-map/state/payload-models/RfpUiEditDetail.ts
@@ -25,8 +25,8 @@ export interface RfpUiEditDetailPayload extends BaseModelPayload
    zip:                   string;         /// The zip code component of the geography
    atz:                   string;         /// The atz component of the geography
    cityName:              string;         /// The city / state of the geography
-   household:             number;         /// When bought, hh_count_mbu otherwise hh_count_pref_ihd. If null, then hh_count_geo
-   taHousehold:           number;         /// Households in the trade area
+   households:             number;         /// When bought, hh_count_mbu otherwise hh_count_pref_ihd. If null, then hh_count_geo
+   taHouseholds:           number;         /// Households in the trade area
    distribution:          number;         /// The distribution (circ)
    addOnDistribution:     number;
    cpm:                   number;         /// Average MBU price (cost per thousand)
