ee7cbb85db8d72f61b0dfe4761c4e01276b24023
Naveen Reddy <reddyn@reddyn-mac-l.val.vlss.local>
2019-11-01 10:37:38 -0400

    IMPWR-14321:Delete Custom Data

--- applications/impower/app/components/target-audience/custom-audience/custom-audience.component.ts
+++ applications/impower/app/components/target-audience/custom-audience/custom-audience.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { FileUpload } from 'primeng/fileupload';
 import * as xlsx from 'xlsx';
 import { Store } from '@ngrx/store';
@@ -6,19 +6,32 @@ import { LocalAppState } from '../../../state/app.interfaces';
 import { ErrorNotification, StartBusyIndicator, StopBusyIndicator } from '@val/messaging';
 import { ProjectPrefGroupCodes } from '../../../val-modules/targeting/targeting.enums';
 import { AppProjectPrefService } from '../../../services/app-project-pref.service';
-import { FetchCustom } from 'app/impower-datastore/state/transient/audience/audience.actions';
+import { FetchCustom, DeleteAudiences, SelectMappingAudience } from 'app/impower-datastore/state/transient/audience/audience.actions';
+import * as fromAudienceSelectors from 'app/impower-datastore/state/transient/audience/audience.selectors';
+import { BehaviorSubject } from 'rxjs';
+import { Audience } from 'app/impower-datastore/state/transient/audience/audience.model';
+import { ConfirmationService } from 'primeng/api';
+import { ClearMapVars } from 'app/impower-datastore/state/transient/map-vars/map-vars.actions';
 
 @Component({
   selector: 'val-custom-audience',
   templateUrl: './custom-audience.component.html'
 })
-export class CustomAudienceComponent {
+export class CustomAudienceComponent implements OnInit {
   private readonly spinnerId = 'CUSTOM_UPLOAD';
+  private allAudiencesBS$ = new BehaviorSubject<Audience[]>([]);
+  public audiences: Audience[] = [];
 
   @ViewChild('audienceUpload', { static: true }) private audienceUploadEl: FileUpload;
 
   constructor(private appProjectPrefService: AppProjectPrefService,
-              private store$: Store<LocalAppState>) { }
+    private confirmationService: ConfirmationService,
+              private store$: Store<LocalAppState>) {}
+  
+  ngOnInit() { 
+    this.store$.select(fromAudienceSelectors.getAllAudiences).subscribe(audiences => this.audiences = audiences.filter(aud => aud.audienceSourceType === 'Custom'));
+    //this.audiences = this.allAudiencesBS$.value.filter(aud => aud.audienceSourceType === 'Custom');
+  }            
 
   public uploadFile(event: any) : void {
     const reader = new FileReader();
@@ -67,5 +80,31 @@ export class CustomAudienceComponent {
     this.audienceUploadEl.clear();
     // workaround for https://github.com/primefaces/primeng/issues/4816
     this.audienceUploadEl.basicFileInput.nativeElement.value = '';
+  }
+
+  public deleteCustomData(){
+    const ids: string[] = [];
+    let audience: Audience = null;
+    this.audiences.forEach(aud => {
+      if (aud.showOnMap == true){
+        aud.showOnMap = false;
+        audience = aud;
+      }
+      
+      ids.push(aud.audienceIdentifier);
+    });
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete all custom data?',
+      header: 'Delete Custom Data',
+      icon: 'ui-icon-delete',
+      accept: () => {
+        if (audience != null && audience.showOnMap){
+          this.store$.dispatch(new ClearMapVars());
+          this.store$.dispatch(new SelectMappingAudience({ audienceIdentifier: audience.audienceIdentifier, isActive: audience.showOnMap }));
+        }
+        this.store$.dispatch(new DeleteAudiences({ids: ids}));
+      },
+      reject: () => {}
+    });
   }
 }
