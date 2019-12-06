734aea4a5112e1b8cefe70699b3132a5d4d18775
Naveen Reddy <reddyn@reddyn-mac-l.val.vlss.local>
2019-12-06 17:02:45 -0500

    IMPWR-14221:Missing Selected variables & Shaded by Vars in the Geo Popup

--- applications/impower/app/services/app-component-generator.service.ts
+++ applications/impower/app/services/app-component-generator.service.ts
@@ -1,6 +1,6 @@
 import { ComponentFactoryResolver, ComponentRef, Injectable, Injector } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { distinctUntilChanged, filter, map, tap } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map, tap, withLatestFrom } from 'rxjs/operators';
 import { ImpGeofootprintVar } from '../val-modules/targeting/models/ImpGeofootprintVar';
 import { ImpGeofootprintVarService } from '../val-modules/targeting/services/ImpGeofootprintVar.service';
 import { AppLoggingService } from './app-logging.service';
@@ -10,15 +10,22 @@ import { ImpProject } from '../val-modules/targeting/models/ImpProject';
 import { ValMetricsService } from './app-metrics.service';
 import { CustomPopUpDefinition, EsriGeographyPopupComponent, NodeVariable } from '@val/esri';
 import { filterArray, mapArray, mapBy, safe } from '@val/common';
+import { FullAppState } from 'app/state/app.interfaces';
+import { Store } from '@ngrx/store';
+import { getMapVars } from 'app/impower-datastore/state/transient/map-vars/map-vars.selectors';
+import { getAllAudiences, getAudiencesOnMap } from 'app/impower-datastore/state/transient/audience/audience.selectors';
+import { MapVar } from 'app/impower-datastore/state/transient/map-vars/map-vars.model';
+import { Audience } from 'app/impower-datastore/state/transient/audience/audience.model';
+import { ImpProjectVar } from 'app/val-modules/targeting/models/ImpProjectVar';
 
-const convertToNodeVariable = (variable: ImpGeofootprintVar, projectVarsDict: any) : NodeVariable => {
-  const fieldType = ((projectVarsDict[variable.varPk] || safe).fieldconte || '').toUpperCase();
+const convertToNodeVariable = (mapVar: MapVar, audience: Audience, impVar: ImpProjectVar) : NodeVariable => {
+  const fieldType = audience.fieldconte.toUpperCase();
   const digits: number = fieldType === 'RATIO' || fieldType === 'PERCENT' ? 2 : 0;
   return {
-    value: variable.value, //(projectVarsDict[variable.varPk]||safe).isNumber ? variable.valueNumber : variable.valueString,
+    value: mapVar[audience.audienceIdentifier], //(projectVarsDict[variable.varPk]||safe).isNumber ? variable.valueNumber : variable.valueString,
     digitRounding: digits,
-    isNumber: (projectVarsDict[variable.varPk] || safe).isNumber,
-    name: (projectVarsDict[variable.varPk] || safe).customVarExprDisplay
+    isNumber: impVar.isNumber,
+    name: impVar.customVarExprDisplay
   };
 };
 
@@ -32,14 +39,29 @@ export class AppComponentGeneratorService {
   private shadingSub: Subscription;
   private audienceSub: Subscription;
 
+  private mapVars: MapVar[] = [];
+  private audiences: Audience[] = [];
+
   constructor(private appStateService: AppStateService,
               private targetAudienceService: TargetAudienceService,
               private impGeofootprintVarService: ImpGeofootprintVarService,
               private logger: AppLoggingService,
               private resolver: ComponentFactoryResolver,
               private injector: Injector,
-              private valMetricsService: ValMetricsService) {
+              private valMetricsService: ValMetricsService,
+              private store$: Store<FullAppState>) {
     this.appStateService.refreshDynamicContent$.subscribe(() => this.updateDynamicComponents());
+
+     this.store$.select(getMapVars).pipe(
+      filter(mapVars => mapVars.length > 0),
+    ).subscribe(data => this.mapVars = data);
+
+    this.store$.select(getAudiencesOnMap).pipe(
+      filter(audiences => audiences.length > 0),
+    ).subscribe(data => this.audiences = data);
+
+    
+
   }
 
   public geographyPopupFactory(feature: __esri.Feature, fields: __esri.FieldInfo[], popupDefinition: CustomPopUpDefinition) : HTMLElement {
@@ -74,8 +96,14 @@ export class AppComponentGeneratorService {
 
   private populateGeographyPopupData(geocode: any, feature: __esri.Feature, fields: __esri.FieldInfo[], popupDefinition: CustomPopUpDefinition, component: ComponentRef<EsriGeographyPopupComponent>) : void {
     this.logger.debug.log('Setting popup values', geocode, feature.graphic.attributes, fields, popupDefinition);
-    if (this.shadingSub) this.shadingSub.unsubscribe();
-    this.shadingSub = this.targetAudienceService.shadingData$.pipe(
+    const mapVars: MapVar[] = this.mapVars.filter(mapVar => mapVar.geocode === geocode);
+    const impProjectVars: ImpProjectVar[] = this.appStateService.currentProject$.getValue().impProjectVars.filter(impVar => impVar.isShadedOnMap);
+
+    if (this.cachedGeoPopup != null && this.cachedGeoPopup.size > 0 && mapVars.length > 0)
+       this.cachedGeoPopup.get(geocode).instance.mapVar = convertToNodeVariable(mapVars[0], this.audiences[0], impProjectVars[0]);
+
+    
+    /*this.shadingSub = this.targetAudienceService.shadingData$.pipe(
       filter(dataDictionary => dataDictionary.has(geocode) && this.cachedGeoPopup != null),
       map(dataDictionary => dataDictionary.get(geocode)),
       distinctUntilChanged(),
@@ -90,7 +118,7 @@ export class AppComponentGeneratorService {
     ).subscribe(nodeData => {
       if ( this.cachedGeoPopup.has(geocode)) 
         this.cachedGeoPopup.get(geocode).instance.geoVars = nodeData;
-    }); 
+    }); */
 
     const invest = this.calcInvestment(feature.graphic.attributes);
     feature.graphic.setAttribute('Investment', invest != null && invest !== '0.00' ? `$${invest}` : 'N/A');
