703a147c367417bcedc4008120bdc0dd770c43d8
Kirk Clawson <kirk.clawson@outlook.com>
2020-01-16 09:28:20 -0500

    CPQ Maps - Fix for mini media plan

--- applications/cpq-maps/src/app/cpq-map/services/entity-helper-service.ts
+++ applications/cpq-maps/src/app/cpq-map/services/entity-helper-service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { EMPTY, merge, Observable } from 'rxjs';
+import { map, reduce, tap } from 'rxjs/operators';
+import { MediaPlanPref } from 'src/app/val-modules/mediaexpress/models/MediaPlanPref';
 import { DAOBaseStatus } from '../../val-modules/api/models/BaseModel';
 import { RestDataService } from '../../val-modules/common/services/restdata.service';
-import { RestResponse } from '../models/RestResponse';
-import { RfpUiEditLoaderService } from './rfpUiEdit-loader-service';
-import { RfpUiEditDetailLoaderService } from './RfpUiEditDetail-loader-service';
-import { RfpUiReviewLoaderService } from './rfpUiReview-loader-service';
-import { RfpUiEditWrapLoaderService } from './rfpUiEditWrap-loader-service';
-import { LocalState } from '../state';
-import { Store } from '@ngrx/store';
-import { SetIsWrap } from '../state/shared/shared.actions';
-import { Observable, merge, EMPTY } from 'rxjs';
 import { NormalizedPayload } from '../models/NormalizedPayload';
-import { map, reduce, tap } from 'rxjs/operators';
+import { RestResponse } from '../models/RestResponse';
+import { LocalState } from '../state';
+import { SetIsWrap } from '../state/shared/shared.actions';
 import { MediaPlanGroupLoaderService } from './mediaplanGroup-loader-service';
 import { MediaPlanPrefLoaderService } from './mediaplanPref-loader-service';
-import { MediaPlanPref } from 'src/app/val-modules/mediaexpress/models/MediaPlanPref';
+import { RfpUiEditLoaderService } from './rfpUiEdit-loader-service';
+import { RfpUiEditDetailLoaderService } from './RfpUiEditDetail-loader-service';
+import { RfpUiEditWrapLoaderService } from './rfpUiEditWrap-loader-service';
+import { RfpUiReviewLoaderService } from './rfpUiReview-loader-service';
 
 @Injectable({
   providedIn: 'root'
@@ -88,7 +88,7 @@ export class EntityHelper {
   }
 
   private prepUpdates(state: LocalState) : Observable<RestResponse> {
-    const updates = state.shared.editedLineItemIds.map(id => state.rfpUiEditDetail.entities[id]).filter(rfp => rfp.commonMbuId == null);
+    const updates = state.shared.editedLineItemIds.map(id => state.rfpUiEditDetail.entities[id]).filter(rfp => rfp.commonMbuId != null);
     const updatePayload = updates.map(u => ({ id: u.commonMbuId, value: u.isSelected }));
     return updates.length > 0
       ? this.restService.post(this.setSelectedUrl, updatePayload)
@@ -96,7 +96,7 @@ export class EntityHelper {
   }
 
   private prepMiniMediaPlan(state: LocalState) : Observable<RestResponse> {
-    const adds = state.shared.newLineItemIds.map(id => state.rfpUiEditDetail.entities[id]).filter(rfp => rfp.commonMbuId != null);
+    const adds = state.shared.newLineItemIds.map(id => state.rfpUiEditDetail.entities[id]).filter(rfp => rfp.commonMbuId == null);
     const addPayload =  {
       products: [{
         product: {
