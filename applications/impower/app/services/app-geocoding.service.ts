33152dc5f13101da1fa8aaef6a3a9c13b1926886
Naveen Reddy <reddyn@reddyn-mac-l.val.vlss.local>
2020-03-04 13:44:41 -0500

    IMPWR-14668:File upload with 0/null radius files

--- applications/impower/app/services/app-geocoding.service.ts
+++ applications/impower/app/services/app-geocoding.service.ts
@@ -33,7 +33,7 @@ export class AppGeocodingService {
     let result = [];
     try {
       const data: ParseResponse<ValGeocodingRequest> = FileService.parseDelimitedData(header, dataRows, parser, this.duplicateKeyMap.get(siteType));
-      if (data.duplicateHeaders.length > 0 || data.invalidColLengthHeaders.length > 0){
+      if (data != null && (data.duplicateHeaders.length > 0 || data.invalidColLengthHeaders.length > 0)){
         if (data.duplicateHeaders.length > 0)
             this.store$.dispatch(new ErrorNotification({ message: 'The upload file contains duplicate headers, please fix the file and upload again.', notificationTitle: 'Duplicate Headers' }));
         if (data.invalidColLengthHeaders.length > 0)
@@ -41,7 +41,7 @@ export class AppGeocodingService {
         //throw new Error();
         data.parsedData = [];
       }
-      if (Object.keys(data.invalidRowHeaders).length > 0){
+      if (data != null && Object.keys(data.invalidRowHeaders).length > 0){
 
         Object.keys(data.invalidRowHeaders).map((headerCol) => {
           //console.log('index values====>', record[headerCol], headerCol);
