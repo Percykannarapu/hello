b59816b26665a4f962b7ee1c70a625be1297b007
kirk-clawson <kirk.clawson@outlook.com>
2020-04-16 14:02:10 -0400

    IMPWR-14816 Investigate webpack feature

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,8 +1,8 @@
 const ArcGISPlugin = require("@arcgis/webpack-plugin");
 const webpack = require('webpack');
 const dotenv = require('dotenv').config({path: __dirname + '/.env'});
-
-console.log('Webpack config started. dotenv Url is set:', dotenv.parsed.ESRI_PORTAL_SERVER != null);
+const parsedEnv = dotenv.parsed || {};
+console.log('Webpack config started. dotenv Url is set:', parsedEnv.ESRI_PORTAL_SERVER != null);
 
 /**
  * Configuration items defined here will be appended to the end of the existing webpack config defined by the Angular CLI.
@@ -10,9 +10,9 @@ console.log('Webpack config started. dotenv Url is set:', dotenv.parsed.ESRI_POR
 module.exports = {
   plugins: [
     new webpack.EnvironmentPlugin({
-      'ESRI_PORTAL_SERVER': dotenv.parsed.ESRI_PORTAL_SERVER,
-      'ESRI_USERNAME': dotenv.parsed.ESRI_USERNAME,
-      'ESRI_PASSWORD': dotenv.parsed.ESRI_PASSWORD
+      'ESRI_PORTAL_SERVER': parsedEnv.ESRI_PORTAL_SERVER,
+      'ESRI_USERNAME': parsedEnv.ESRI_USERNAME,
+      'ESRI_PASSWORD': parsedEnv.ESRI_PASSWORD
     }),
     new ArcGISPlugin({
       options: {
