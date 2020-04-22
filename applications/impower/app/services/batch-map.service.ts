056cb304676439052f227144937ca9b20ba1745f
Cirillo, Aaron <amcirillo@valassis.com>
2020-04-22 13:09:42 -0400

    IMPWR-14503: stripping inactive sites from print requests

--- applications/impower/app/services/batch-map.service.ts
+++ applications/impower/app/services/batch-map.service.ts
@@ -69,8 +69,8 @@ export class BatchMapService {
     if (payload.calls[0].args['printJobConfiguration'] != null) {
       const requestedSiteIds = new Set(payload.calls[0].args['printJobConfiguration'].siteIds);
       project.getImpGeofootprintLocations().forEach( l => {
-      if ((l.clientLocationTypeCode === 'Failed Site' || l.clientLocationTypeCode === ImpClientLocationTypeCodes.Competitor) && requestedSiteIds.has(l.locationNumber))
-        requestedSiteIds.delete(l.locationNumber); //we don't want to print failed sites
+      if ((l.clientLocationTypeCode === 'Failed Site' || l.clientLocationTypeCode === ImpClientLocationTypeCodes.Competitor || !l.isActive) && requestedSiteIds.has(l.locationNumber))
+        requestedSiteIds.delete(l.locationNumber); //we don't want to print failed sites, competitors, or inactive sites
       });
       payload.calls[0].args['printJobConfiguration'].siteIds = Array.from(requestedSiteIds);
     }
