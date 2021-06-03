0bc8af7a173c833c2e66d1773a11ec7774f1b3cc
Kannarapu, Percy <KannarapuP@valassis.com>
2021-06-03 10:22:47 -0500

    IMPWR-15214: Add toolTip, delimiters and validations for manual add must covers
    Squashed commit of the following:
    
    commit 8841742b2b1597ac11a9e59137a59ce809315e0d
    Author: Kannarapu, Percy <kannarapup@valassis.com>
    Date:   Thu Jun 3 11:20:12 2021 -0400
    
        IMPWR-15214: Fix typo in tooltip
    
    commit 0ca7db97ec8a1086cfbd0f052f815fb6089b873e
    Author: Kannarapu, Percy <kannarapup@valassis.com>
    Date:   Thu Jun 3 11:13:55 2021 -0400
    
        IMPWR-15214: Add toolTip, delimiters and validations for manual add must covers

--- applications/impower/app/components/trade-area-tab/upload-must-cover/upload-must-cover.component.ts
+++ applications/impower/app/components/trade-area-tab/upload-must-cover/upload-must-cover.component.ts
@@ -32,6 +32,7 @@ export class UploadMustCoverComponent implements OnInit {
    public isDisable: boolean = true;
    public isUpload: boolean = true;
    public tooltip;
+   public manualToolTip;
    public currentAnalysisLevel$: Observable<string>;
    public totalUploadedRowCount = 0;
    private fileName: string;
@@ -241,6 +242,7 @@ export class UploadMustCoverComponent implements OnInit {
       this.fileAnalysisSelected = event;
       this.fileAnalysisLabel = this.allAnalysisLevels.filter(analysis => analysis.value === this.fileAnalysisSelected)[0].label;
       this.tooltip = 'CSV or Excel format, required field is Geocode';
+      this.manualToolTip = 'Please enter geocodes separated by comma or carriage return.';
    }
 
    deleteMustCovers(){
@@ -283,12 +285,11 @@ export class UploadMustCoverComponent implements OnInit {
       const analysisLevel = this.appStateService.analysisLevel$.getValue();
       const manualMustCoverGeos = this.mustCoverGeos.split(/,|,\s|\r\n|\n|\r/);
       manualMustCoverGeos.forEach(geo => {
-         geo.replace(" ","");
-         if(this.fileAnalysisSelected === 'ZIP' && geo.match(/^[0-9]{5}$/) || (this.fileAnalysisSelected === 'ATZ' && geo.match(/^[0-9]{5}|[0-9]{5}[A-Z]{1}?[0-9]{1}?$/)) ||
-               (this.fileAnalysisSelected === 'Digital ATZ' && geo.match(/^[0-9]{5,9}?$/)) || (this.fileAnalysisSelected === 'PCR' && geo.match(/^[0-9]{5}[A-Z]{1}[0-9]{3}$/)) ||
-                  (this.fileAnalysisSelected === 'WRAP_MKT_ID' && geo.match(/^[A-Z]{1,8}$/)) || (this.fileAnalysisSelected === 'COUNTY' && geo.match(/^[a-zA-Z ]*$/)) ||
-                   (this.fileAnalysisSelected === 'STATE' && geo.match(/^[A-Za-z]{2}$/)) || (this.fileAnalysisSelected === 'DMA' && geo.match(/^[0-9]{4}$/)) || 
-                     (this.fileAnalysisSelected === 'INFOSCAN_CODE' && geo.match(/^[0-9]{3}$/)) || (this.fileAnalysisSelected === 'SCANTRACK_CODE')){
+         if(this.fileAnalysisSelected === 'ZIP' && geo.match(/^\s*[0-9]{5}\s*$/) || (this.fileAnalysisSelected === 'ATZ' && geo.match(/^\s*[0-9]{5}|[0-9]{5}[A-Z]{1}?[0-9]{1}?\s*$/)) ||
+               (this.fileAnalysisSelected === 'Digital ATZ' && geo.match(/^\s*[0-9]{5,9}?$\s*/)) || (this.fileAnalysisSelected === 'PCR' && geo.match(/^\s*[0-9]{5}[A-Z]{1}[0-9]{3}\s*$/)) ||
+                  (this.fileAnalysisSelected === 'WRAP_MKT_ID' && geo.match(/^\s*[A-Z]{1,8}\s*$/)) || (this.fileAnalysisSelected === 'COUNTY' && geo.match(/^\s*[a-zA-Z ]*\s*$/)) ||
+                   (this.fileAnalysisSelected === 'STATE' && geo.match(/^\s*[A-Za-z]{2}\s*$/)) || (this.fileAnalysisSelected === 'DMA' && geo.match(/^\s*[0-9]{4}\s*$/)) || 
+                     (this.fileAnalysisSelected === 'INFOSCAN_CODE' && geo.match(/^\s*[0-9]{3}\s*$/)) || (this.fileAnalysisSelected === 'SCANTRACK_CODE' && geo.match(/^\s*[0-9]{2}\s*$/) )){
             geos.push(geo);
          }
          else
